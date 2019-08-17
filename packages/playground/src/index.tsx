0b4f2c7c3de3bb77a131df49bdf1512673b6d2a1
Orta Therox <orta.therox@gmail.com>
2019-08-17 19:06:56 -0400

    Add the playground and the playground sandbox

---
+++ packages/playground/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export interface PlaygroundPlugin  {
+  name: string
+}
+
+export const Thing = () => {
+  return <div>the snozzberries taste like snozzberries</div>;
+};
