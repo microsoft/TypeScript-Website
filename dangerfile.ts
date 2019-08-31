1d5278749d0f168f42be944acad450127e28b933
Orta Therox <orta.therox@gmail.com>
2019-08-31 08:26:20 -0400

    Adds danger plugin spellcheck

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,3 +1,14 @@
-import {message} from "danger"
+import {danger} from "danger"
 
-message("Hello world")
+// dangerfile.js
+import spellcheck from 'danger-plugin-spellcheck'
+
+const hasElevatedAccess = danger.github.pr.author_association === "COLLABORATOR" ||
+                          danger.github.pr.author_association === "OWNER"
+
+// Start with the spell-check as a bit less spammy
+if (hasElevatedAccess) {
+  spellcheck({
+    codeSpellCheck: ["Examples/**/*.ts", "Examples/**/*.js"]
+  })
+}
