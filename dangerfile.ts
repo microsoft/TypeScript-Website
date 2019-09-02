4f4db5b20dc1a279a2b0a7c85ae25de12d02f830
Orta Therox <orta.therox@gmail.com>
2019-09-02 19:19:12 -0400

    Make the dangerfile post a comment about the PR build of the site

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,14 +1,16 @@
-import {danger} from "danger"
+import {danger, message} from "danger"
 
 // dangerfile.js
 import spellcheck from 'danger-plugin-spellcheck'
 
 const hasElevatedAccess = danger.github.pr.author_association === "COLLABORATOR" ||
-                          danger.github.pr.author_association === "OWNER"
+                          danger.github.pr.author_association === "OWNER" ||
+                          danger.github.pr.author_association === "MEMBER"
 
 // Start with the spell-check as a bit less spammy
-if (hasElevatedAccess) {
+
   spellcheck({
     codeSpellCheck: ["Examples/**/*.ts", "Examples/**/*.js"]
   })
-}
+
+  message(`Deployed to: https://typescript-playbook-${danger.github.pr.number}.ortam.now.sh`) 
