1cfd0704ff881180b3a22487409c8941ddf183a4
Orta Therox <orta.therox@gmail.com>
2019-11-08 20:17:00 -0500

    Adds a better mobile nav

--- packages/typescriptlang-org/src/components/layout.tsx
+++ packages/typescriptlang-org/src/components/layout.tsx
@@ -1,18 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
 import { SiteNav } from "./layout/TopNav"
 
-
-declare const __PATH_PREFIX__: string
 
 export class Layout extends React.Component<any> {
   render() {
     const { children } = this.props
     return (
-      <div>
-        <SiteNav  />
+      <>
+        <SiteNav />
         <main>{children}</main>
-      </div>
+      </>
     )
   }
 }
