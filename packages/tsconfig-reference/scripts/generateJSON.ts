8c62b6ac27fec8ef25dd08542596f6b8b2401fa6
Orta Therox <orta.therox@gmail.com>
2019-11-15 11:12:03 -0500

    Fix build

--- packages/tsconfig-reference/scripts/generateJSON.ts
+++ packages/tsconfig-reference/scripts/generateJSON.ts
@@ -135,7 +135,7 @@ const topLevelTSConfigOptions = [
 ]
 
 
-writeJSON('tsconfigOpts.json', [...topLevelTSConfigOptions, ...filteredOptions])
+writeJSON('tsconfigOpts.json', { options: [...topLevelTSConfigOptions, ...filteredOptions]})
 
 // Improve the typing for the rules
 writeString(
