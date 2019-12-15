22b84c2d3ddc268982df0ed407857a0955478e05
Orta Therox <orta.therox@gmail.com>
2019-12-15 11:21:35 +0000

    Try to do sub-token LSP lookups

--- packages/gatsby-remark-shiki/renderer.js
+++ packages/gatsby-remark-shiki/renderer.js
@@ -1,25 +1,28 @@
+// @ts-check
 // This started as a JS port of https://github.com/octref/shiki/blob/master/packages/shiki/src/renderer.ts
 
 /**
  *
- * @param {{ content: string, color?: string }[][]} lines
+ * @param {{ content: string, color?: string, explanation?: any[] }[][]} lines
  * @param {{ langId: string }} options
  * @param { import("ts-twoslasher").TwoSlashReturn } twoslash
  */
 function renderToHTML(lines, options, twoslash) {
   const bg = options.bg || '#f8f8f8'
-
+  if (!twoslash) {
+    return plainOleShikiRenderer(lines, options)
+  }
   let html = ''
 
-  html += `<pre class="shiki">`
+  html += `<pre class="shiki twoslash">`
   if (options.langId) {
     html += `<div class="language-id">${options.langId}</div>`
   }
   html += `<div class='code-container'><code>`
 
-  const errorsGroupedByLine = (twoslash && groupBy(twoslash.errors, e => e.line)) || new Set()
-  const staticQuickInfosGroupedByLine = (twoslash && groupBy(twoslash.staticQuickInfos, q => q.line)) || new Set()
-  const queriesGroupedByLine = (twoslash && groupBy(twoslash.queries, q => q.line)) || new Set()
+  const errorsGroupedByLine = (twoslash && groupBy(twoslash.errors, e => e.line)) || new Map()
+  const staticQuickInfosGroupedByLine = (twoslash && groupBy(twoslash.staticQuickInfos, q => q.line)) || new Map()
+  const queriesGroupedByLine = (twoslash && groupBy(twoslash.queries, q => q.line)) || new Map()
 
   lines.forEach((l, i) => {
     const errors = errorsGroupedByLine.get(i) || []
@@ -28,26 +31,65 @@ function renderToHTML(lines, options, twoslash) {
     if (l.length === 0) {
       html += `\n`
     } else {
-      // Keep track of the position of the current token in a line so we can match it up to the errors and
-      // lang serv identifiers
+      // Keep track of the position of the current token in a line so we can match it up to the
+      // errors and lang serv identifiers
       let pos = 0
 
       l.forEach(token => {
         // Underlining particular words
-        const findTokenFunc = e => e.character <= pos && e.character + e.length >= pos + token.content.length
-        // const debugLoggingTokenFunc = e => {
-        //   console.log(e.character, '<=', pos, '&&', e.character + e.length, '>=', pos + token.content.length)
-        //   return e.character <= pos && e.character + e.length >= pos + token.content.length
-        // }
+        const findTokenFunc = start => e =>
+          start <= e.character && start + token.content.length <= e.character + e.length
 
-        const isError = errors.find(findTokenFunc)
-        const tokenLSPResponse = lspValues.find(findTokenFunc)
+        const isTokenFunc = start => e =>
+          start === e.character && start + token.content.length === e.character + e.length
 
-        const className = isError ? "class='err'" : ''
-        const tokenLSPResponseString = tokenLSPResponse ? `data-lsp='${tokenLSPResponse.text}'` : ''
+        const isInsideTokenDebug = start => e => {
+          const result = start <= e.character && start + token.content.length <= e.character + e.length
+          // prettier-ignore
+          console.log(result, start, '<=', e.character, '&&', start + token.content.length, '<=', e.character + e.length)
+          return result
+        }
 
-        const content = escapeHtml(token.content)
-        html += `<span ${className} style="color: ${token.color}" ${tokenLSPResponseString}>${content}</span>`
+        const errorsInToken = errors.filter(findTokenFunc(pos))
+        const lspResponsesInToken = lspValues.filter(findTokenFunc(pos))
+
+        // The explanation is all of the token which make up this "meta token"
+        // these meta tokens are just the consolidation of many tokens which share the same color
+
+        const subtokens = token.explanation || [{ content: token.content }]
+        let subpos = pos
+        subtokens.forEach(subtoken => {
+          // If there are errors or LSP results (or TODO: highlights) then wrap the token with a
+          // span which has the right classes and LSP results
+          const classes = []
+          const extras = []
+
+          const errorsInSubToken = errorsInToken.find(isTokenFunc(subpos))
+          if (errorsInSubToken) {
+            classes.push('err')
+          }
+
+          const lspResponsesInSubToken = lspResponsesInToken.find(isTokenFunc(subpos))
+          if (lspResponsesInSubToken) {
+            classes.push('lsp')
+            extras.push(`data-lsp='${encodeURIComponent(lspResponsesInSubToken.text)}'`)
+          }
+
+          // We want to move the subpos along by the real content because we're about to manipulate that
+          // this needs to happen after the finds above
+          subpos += subtoken.content.length
+
+          const content = escapeHtml(subtoken.content)
+          if (classes.length === 0) {
+            subtoken.content = content
+          } else {
+            subtoken.content = `<span a class='${classes.join(' ')}' ${extras.join(' ')}>${content}</span>`
+          }
+        })
+
+        const content = subtokens.map(t => t.content).join('')
+        html += `<span style="color: ${token.color}">${content}</span>`
+
         pos += token.content.length
       })
 
@@ -87,6 +129,36 @@ function groupBy(list, keyGetter) {
   return map
 }
 
+/**
+ *
+ * @param {{ content: string, color?: string, explanation?: any[]  }[][]} lines
+ * @param {{ langId: string }} options
+ */
+function plainOleShikiRenderer(lines, options) {
+  let html = ''
+
+  html += `<pre class="shiki">`
+  if (options.langId) {
+    html += `<div class="language-id">${options.langId}</div>`
+  }
+
+  html += `<div class='code-container'><code>`
+
+  lines.forEach(l => {
+    if (l.length === 0) {
+      html += `\n`
+    } else {
+      l.forEach(token => {
+        html += `<span style="color: ${token.color}">${escapeHtml(token.content)}</span>`
+      })
+    }
+  })
+
+  html += `</code></div></pre>`
+  return html
+}
+
 module.exports = {
   renderToHTML,
+  plainOleShikiRenderer,
 }
--- packages/gatsby-remark-shiki/renderer.test.js
+++ packages/gatsby-remark-shiki/renderer.test.js
@@ -2,14 +2,14 @@ const remark = require('remark')
 const gatsbyRemarkShiki = require('./index')
 const gatsbyTwoSlash = require('gatsby-remark-twoslasher-code-blocks')
 
-const getMarkdownASTForCode = async code => {
+const getMarkdownASTForCode = async (code, settings) => {
   const markdownAST = remark().parse(code)
   gatsbyTwoSlash({ markdownAST })
-  await gatsbyRemarkShiki({ markdownAST })
+  await gatsbyRemarkShiki({ markdownAST }, settings)
   return markdownAST
 }
 
-describe('supports hiding the example code', () => {
+describe('with a simple example', () => {
   const file = `
 hello world
 
@@ -28,6 +28,10 @@ OK world
     // Comes from remark
     expect(code.lang).toEqual('ts')
     expect(code.meta).toEqual('twoslash')
+
+    // Comes from ts-twoslash
+    expect(code.twoslash.extension).toEqual('ts')
+    expect(code.twoslash.staticQuickInfos).toHaveLength(2)
   })
 
   it('shows the right LSP results', async () => {
@@ -35,7 +39,114 @@ OK world
     const code = markdownAST.children[1]
 
     expect(code.value).toContain(`data-lsp`)
-    expect(code.value).toContain(`const a: "123"`)
-    expect(code.value).toContain(`const b: "345"`)
+    expect(code.value).toContain(encodeURIComponent(`const a: "123"`))
+    expect(code.value).toContain(encodeURIComponent(`const b: "345"`))
+  })
+})
+
+describe('with a more complex example', () => {
+  const file = `
+### This will error
+
+\`\`\`ts twoslash
+// @errors: 2345
+function longest<T extends { length: number }>(a: T, b: T) {
+  if (a.length >= b.length) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+// longerArray is of type 'number[]'
+const longerArray = longest([1, 2], [1, 2, 3]);
+// longerString is of type 'string'
+const longerString = longest("alice", "bob");
+// Error! Numbers don't have a 'length' property
+const notOK = longest(10, 100);
+
+const hello = longest("alice", "bob");
+console.log(hello);
+
+\`\`\`
+
+OK world
+`
+
+  it('shows the right LSP results', async () => {
+    const markdownAST = await getMarkdownASTForCode(file)
+    const code = markdownAST.children[1]
+
+    expect(code.twoslash.extension).toEqual('ts')
+    expect(code.twoslash.staticQuickInfos.length).toBeGreaterThan(1)
+
+    expect(code.value).toContain(`data-lsp`)
+    expect(code.value).toContain(encodeURIComponent(`function longest<T extends {`))
+
+    // Error message
+    expect(code.value).toContain(`span class="error"`)
+    expect(code.value).toContain(`span class="error-behind"`)
+    // The error code
+    expect(code.value).toContain(`<span class="code">2345</span>`)
+  })
+
+  it('shows the right LSP results when a theme doesnt have unique tokens for identifiers', async () => {
+    const markdownAST = await getMarkdownASTForCode(file, { theme: 'light_vs' })
+    const code = markdownAST.children[1]
+
+    expect(code.value).toContain(`data-lsp`)
+    expect(code.value).toContain(encodeURIComponent(`function longest<T extends`))
+
+    // Error message
+    expect(code.value).toContain(`span class="error"`)
+    expect(code.value).toContain(`span class="error-behind"`)
+    // The error code
+    expect(code.value).toContain(`<span class="code">2345</span>`)
+  })
+})
+
+describe('raw LSP details example', () => {
+  const file = `
+### This will error
+
+\`\`\`ts twoslash
+function longest() {}
+\`\`\`
+
+OK world
+`
+
+  it('shows the right LSP results when a theme doesnt have unique tokens for identifiers', async () => {
+    const markdownAST = await getMarkdownASTForCode(file, { theme: 'light_vs' })
+    const code = markdownAST.children[1]
+
+    expect(code.value).toContain(`data-lsp`)
+    expect(code.value).toContain(encodeURIComponent(`function longest()`))
+
+    expect(code.value.split('data-lsp').length).toEqual(code.twoslash.staticQuickInfos.length + 1)
+  })
+})
+
+describe('no twoslash', () => {
+  const file = `
+### This should not get twoslashd
+
+\`\`\`js
+function longest() {
+
+}
+\`\`\`
+
+OK world
+`
+
+  it('looks about right', async () => {
+    const markdownAST = await getMarkdownASTForCode(file, { theme: 'light_vs' })
+    const code = markdownAST.children[1]
+    expect(code.value).toContain('\n')
+    expect(code.value).toMatchInlineSnapshot(`
+      "<pre class=\\"shiki\\"><div class=\\"language-id\\">js</div><div class='code-container'><code><span style=\\"color: #81A1C1\\">function</span><span style=\\"color: #D8DEE9FF\\"> </span><span style=\\"color: #88C0D0\\">longest</span><span style=\\"color: #ECEFF4\\">()</span><span style=\\"color: #D8DEE9FF\\"> </span><span style=\\"color: #ECEFF4\\">{</span>
+      <span style=\\"color: #ECEFF4\\">}</span></code></div></pre>"
+    `)
   })
 })
