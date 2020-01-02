c6c26e63bd60f8116f050362c4c92c3182019bbe
Orta Therox <orta.therox@gmail.com>
2020-01-02 14:33:18 +0000

    Create the exaples dir

--- packages/playground-examples/scripts/copyFiles.js
+++ packages/playground-examples/scripts/copyFiles.js
@@ -7,6 +7,12 @@ const copyDir = join(__dirname, '..', 'copy', '*')
 const jsonDir = join(__dirname, '..', 'generated', '*.json')
 const outDir = join(__dirname, '..', '..', 'typescriptlang-org', 'static', 'js', 'examples')
 
+try {
+  execSync(`mkdir ${outDir}`)
+} catch (error) {
+  // This can safely fail
+}
+
 // Move samples
 execSync(`cp -R ${copyDir} ${outDir}`)
 
