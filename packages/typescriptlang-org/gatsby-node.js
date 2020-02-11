4aaed2c09629ee6399936f155c87bcff64f382a2
Orta Therox <orta.therox@gmail.com>
2020-02-11 15:54:52 -0500

    intl

--- packages/typescriptlang-org/gatsby-node.js
+++ packages/typescriptlang-org/gatsby-node.js
@@ -1,12 +1,35 @@
-
 const { createPages } = require("./lib/bootup/createPages")
 const { onCreateNode } = require("./lib/bootup/onCreateNode")
 
 /** @type { import("gatsby").GatsbyNode } */
-const config = {};
+const config = {}
 exports.config = config
 
 config.createPages = createPages
 config.onCreateNode = onCreateNode
+
+// To ensure canvas (used by JSDom) doesn't break builds during SSR
+// see: https://github.com/gatsbyjs/gatsby/issues/17661
+
+config.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /canvas/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
 
 module.exports = config
--- packages/typescriptlang-org/gatsby-ssr.js
+++ packages/typescriptlang-org/gatsby-ssr.js
@@ -1,78 +1,10 @@
+// This file exists to ensure that global.DOMParser
+// is set up, to let react-intl do it's work with RichText in
+// a message: https://github.com/formatjs/react-intl/issues/1438#issuecomment-523153456
+
+global.DOMParser = new (require("jsdom").JSDOM)().window.DOMParser
+
 const React = require("react")
-const browserLang = require("browser-lang")
-const { withPrefix } = require("gatsby")
-const { IntlProvider } = require("react-intl")
-
-const preferDefault = m => (m && m.default) || m
-
-const polyfillIntl = language => {
-  const locale = language.split("-")[0]
-  try {
-    if (!Intl.PluralRules) {
-      require("@formatjs/intl-pluralrules/polyfill")
-      require(`@formatjs/intl-pluralrules/dist/locale-data/${locale}`)
-    }
-
-    if (!Intl.RelativeTimeFormat) {
-      require("@formatjs/intl-relativetimeformat/polyfill")
-      require(`@formatjs/intl-relativetimeformat/dist/locale-data/${locale}`)
-    }
-  } catch (e) {
-    throw new Error(`Cannot find react-intl/locale-data/${language}`)
-  }
-}
-
-const withIntlProvider = intl => children => {
-  polyfillIntl(intl.language)
-  return (
-    <IntlProvider locale={intl.language} messages={intl.messages}>
-      <IntlContextProvider value={intl}>{children}</IntlContextProvider>
-    </IntlProvider>
-  )
-}
-
-export default ({ element, props }) => {
-  if (!props) {
-    return
-  }
-
-  const { pageContext, location } = props
-  const { intl } = pageContext
-  const { language, languages, redirect, routed, originalPath } = intl
-
-  if (typeof window !== "undefined") {
-    window.___gatsbyIntl = intl
-  }
-  /* eslint-disable no-undef */
-  const isRedirect = redirect && !routed
-
-  if (isRedirect) {
-    const { search } = location
-
-    // Skip build, Browsers only
-    if (typeof window !== "undefined") {
-      let detected =
-        window.localStorage.getItem("gatsby-intl-language") ||
-        browserLang({
-          languages,
-          fallback: language,
-        })
-
-      if (!languages.includes(detected)) {
-        detected = language
-      }
-
-      const queryParams = search || ""
-      const newUrl = withPrefix(`/${detected}${originalPath}${queryParams}`)
-      window.localStorage.setItem("gatsby-intl-language", detected)
-      window.location.replace(newUrl)
-    }
-  }
-  const renderElement = isRedirect
-    ? GATSBY_INTL_REDIRECT_COMPONENT_PATH &&
-      React.createElement(
-        preferDefault(require(GATSBY_INTL_REDIRECT_COMPONENT_PATH))
-      )
-    : element
-  return withIntlProvider(intl)(renderElement)
+exports.wrapRootElement = ({ element }) => {
+  return <>{element}</>
 }
