38b0fda46c310067ffd5775deb9f1fa37c66af12
Orta Therox <orta.therox@gmail.com>
2020-02-11 17:12:38 -0500

    Fix yarn start

--- packages/typescriptlang-org/gatsby-node.js
+++ packages/typescriptlang-org/gatsby-node.js
@@ -11,25 +11,17 @@ config.onCreateNode = onCreateNode
 // To ensure canvas (used by JSDom) doesn't break builds during SSR
 // see: https://github.com/gatsbyjs/gatsby/issues/17661
 
-config.onCreateWebpackConfig = ({
-  stage,
-  rules,
-  loaders,
-  plugins,
-  actions,
-}) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /canvas/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
+config.onCreateWebpackConfig = ({ loaders, actions }) => {
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /canvas/,
+          use: loaders.null(),
+        },
+      ],
+    },
+  })
 }
 
 module.exports = config
