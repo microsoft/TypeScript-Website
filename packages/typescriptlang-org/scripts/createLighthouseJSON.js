079e993cb16ffbfbfe86c742460eeedd479ccdfb
Orta Therox <orta.therox@gmail.com>
2020-02-14 10:02:48 -0500

    Reduce the number of checks

--- packages/typescriptlang-org/scripts/createLighthouseJSON.js
+++ packages/typescriptlang-org/scripts/createLighthouseJSON.js
@@ -19,8 +19,9 @@ const go = async () => {
     const contents = await packageJSON.text()
 
     const sitemapJSON = JSON.parse(parser.toJson(contents))
-    const urls = sitemapJSON.urlset.url
-      .map(u => u.loc)
+    /** @type {string[]} */
+    const grabbedURLs = sitemapJSON.urlset.url.map(u => u.loc)
+    const urls = grabbedURLs
       .map(url => {
         // from "https://www.typescriptlang.org/v2/docs/handbook/advanced-types.html",
         // to   "https://typescript-v2-" + prNumber + ".ortam.now.sh/docs/handbook/advanced-types.html",
@@ -29,20 +30,45 @@ const go = async () => {
           "https://typescript-v2-" + prNumber + ".ortam.now.sh/"
         )
       })
-      .filter(url => !url.endsWith(".ts"))
       .reverse()
+
+    // Assume that ja/pt/es/etc all have the same issues as the en version
+    const removeNonEnglish = urls =>
+      urls.filter(u => u.includes("/en/") || u.includes("docs"))
+
+    // These aren't user facing, don't spend the time
+    const removeTypeScriptExamples = urls =>
+      urls.filter(u => !u.includes("/play/"))
+
+    // Shuffle the handbooks to cycle through
+    const randomHandbooks = urls => {
+      const handbooks = urls.filter(u => u.includes("/docs/"))
+      return [
+        ...urls.filter(u => !u.includes("/docs/")),
+        handbooks[Math.floor(Math.random() * handbooks.length)],
+        handbooks[Math.floor(Math.random() * handbooks.length)],
+        handbooks[Math.floor(Math.random() * handbooks.length)],
+        handbooks[Math.floor(Math.random() * handbooks.length)],
+        handbooks[Math.floor(Math.random() * handbooks.length)],
+      ]
+    }
 
     const repoRoot = join(__dirname, "..", "..", "..")
     const lighthouseFiles = join(repoRoot, ".lighthouserc.json")
     const json = {
       ci: {
         collect: {
-          url: [...urls],
+          url: [
+            "https://typescript-v2-245.ortam.now.sh/",
+            ...randomHandbooks(
+              removeTypeScriptExamples(removeNonEnglish(urls))
+            ),
+          ],
         },
       },
     }
 
-    console.log(`Looking at ${urls.length} urls`)
+    console.log(`Looking at ${json.ci.collect.url.length} urls`)
     writeFileSync(lighthouseFiles, JSON.stringify(json))
   } catch (error) {
     console.log(
