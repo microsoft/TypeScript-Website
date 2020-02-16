679f034ca3b4d7ab5d5c0afb483cfb62a2be1123
KingDarBoja <stevenbojato04@gmail.com>
2020-02-15 23:03:23 -0500

    Fix copy dir on playground-examples for windows

--- packages/playground-examples/scripts/copyFiles.js
+++ packages/playground-examples/scripts/copyFiles.js
@@ -1,10 +1,11 @@
 // @ts-check
 
 const { execSync } = require('child_process')
-const { join } = require('path')
+const { join, extname } = require('path')
+const fse = require('fs-extra')
 
-const copyDir = join(__dirname, '..', 'copy', '*')
-const jsonDir = join(__dirname, '..', 'generated', '*.json')
+const copyDir = join(__dirname, '..', 'copy')
+const jsonDir = join(__dirname, '..', 'generated')
 const outDir = join(__dirname, '..', '..', 'typescriptlang-org', 'static', 'js', 'examples')
 
 try {
@@ -14,7 +15,16 @@ try {
 }
 
 // Move samples
-execSync(`cp -R ${copyDir} ${outDir}`)
+fse.copySync(copyDir, outDir)
+
+/**
+ * Filter logic, will be included if return true. However, we must skip the source dir as stated on
+ * https://github.com/jprichardson/node-fs-extra/issues/741#issuecomment-579629747
+ */
+
+const filterFunc = src => {
+  return src === jsonDir || extname(src) === '.json'
+}
 
 // Move the JSON files which are generated
-execSync(`cp ${jsonDir} ${outDir}`)
+fse.copySync(jsonDir, outDir, { filter: filterFunc })
