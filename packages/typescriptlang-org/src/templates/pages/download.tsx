97c14ab4f4be560a6d8f7134c70a2bf352e5b59e
Orta Therox <orta.therox@gmail.com>
2020-02-18 15:50:38 -0500

    Some tidying up of the download page

--- packages/typescriptlang-org/src/templates/pages/download.tsx
+++ packages/typescriptlang-org/src/templates/pages/download.tsx
@@ -16,15 +16,16 @@ const Index = (props: any) => {
   return <Layout title="How to set up TypeScript" description="" lang={props.pageContext.lang} allSitePage={props.data.allSitePage}>
     <div className="raised main-content-block">
       <h1>Download TypeScript</h1>
-      <p>TypeScript is available in two main ways depending on how you intend to use it: built as an npm module, and a nuget package.</p>
-      <p>If you are using MSBuild in your project, you want the nuget package, otherwise you will want the npm version.</p>
+      <p>TypeScript can installed through three installation routes depending on how you intend to use it: an npm module, a NuGet package or a Visual Studio Extension.</p>
+      <p>If you are using Node.js, you want the npm version. If you are using MSBuild in your project, you want the NuGet package or Visual Studio extension.</p>
     </div>
 
     <div className="raised main-content-block">
       <h2>TypeScript in Your Project</h2>
-      <p>Having TypeScript set up in a per-project basis lets you have many projects with many different versions of TypeScript, this keeps each project working consistently.</p>
-      <section style={{ display: "flex" }}>
-        <div style={{ borderRight: "1px grey solid", padding: "1rem", flex: 1 }}>
+      <p>Having TypeScript set up on a per-project basis lets you have many projects with many different versions of TypeScript, this keeps each project working consistently.</p>
+
+      <section style={{ display: "flex", flexWrap: "wrap" }}>
+        <div style={{ borderRight: "1px lightgrey solid", padding: "1rem", flex: 1, minWidth: "250px" }}>
           <h3>via NPM</h3>
           <p>TypeScript is available as a <a href="https://www.npmjs.com/package/typescript">package on the npm registry</a> available as <code>"typescript"</code>.</p>
           <p>You will need a copy of <a href="https://nodejs.org/en/">Node.js</a> as an environment to run the package. Then you use a dependency manager like <a href='https://www.npmjs.com/'>npm</a>, <a href='https://yarnpkg.com/'>yarn</a> or <a href='https://pnpm.js.org/'>pnpm</a> to download TypeScript into your project.</p>
@@ -32,15 +33,15 @@ const Index = (props: any) => {
             <code id='code-example'>npm install typescript --save-dev</code><br /><br />
             <button onClick={() => changeExample("npm install typescript --save-dev")}>npm</button> <button onClick={() => changeExample("yarn add typescript --dev")}>yarn</button> <button onClick={() => changeExample("pnpm add typescript -D")}>pnpm</button>
           </div>
-          <p>All of these dependency managers support lockfiles, ensuring that everyone on your team is using the same version of the language. You can then run the TypeScript compiler using one of to the following commands:</p>
+          <p>All of these dependency managers support lockfiles, ensuring that everyone on your team is using the same version of the language. You can then run the TypeScript compiler using one of the following commands:</p>
           <div>
             <code id='code-run'>npx tsc</code><br /><br />
             <button onClick={() => changeExample2("npx tsc")}>npm</button> <button onClick={() => changeExample2("yarn tsc")}>yarn</button> <button onClick={() => changeExample2("pnpx tsc")}>pnpm</button>
           </div>
         </div>
 
-        <div style={{ padding: "1rem", flex: 1 }}>
-          <h2>with Visual Studio</h2>
+        <div style={{ padding: "1rem", flex: 1, minWidth: "250px" }}>
+          <h3>with Visual Studio</h3>
           <p>For most project types, you can get TypeScript as a package in Nuget for your MSBuild projects, for example an ASP.NET Core app.</p>
           <p>When using Nuget, you can <a href="https://docs.microsoft.com/en-us/visualstudio/javascript/tutorial-aspnet-with-typescript">install</a> TypeScript through Visual Studio using:</p>
           <ul>
@@ -48,7 +49,7 @@ const Index = (props: any) => {
               The Manage NuGet Packages window (which you can get to by right-clicking on a project node)
             </li>
             <li style={{ marginTop: "20px" }}>
-              The Nuget Package Manager Console (found in Tools > NuGet Package Manager > Package Manager Console) using <code>Install-Package Microsoft.TypeScript.MSBuild</code>
+              The Nuget Package Manager Console (found in Tools > NuGet Package Manager > Package Manager Console) and then running:<br /><code style={{ fontSize: "14px" }}>Install-Package Microsoft.TypeScript.MSBuild</code>
             </li>
           </ul>
           <p>For project types which don't support Nuget. You can use the <a href={releaseInfo.vs.stable.vs2019_download}> TypeScript Visual Studio extension</a>. You can <a href="https://docs.microsoft.com/en-us/visualstudio/ide/finding-and-using-visual-studio-extensions?view=vs-2019">install</a> the extension using <code>Extensions > Manage Extensions</code> in Visual Studio.</p>
@@ -62,49 +63,48 @@ const Index = (props: any) => {
 
     <div className="raised main-content-block">
       <h2>Globally Installing TypeScript</h2>
-      <p>It can be handy to have TypeScript available across all project, for example to test one-off ideas.</p>
+      <p>It can be handy to have TypeScript available across all projects, for example to test one-off ideas. Installing globally has the drawback of their only being a single version of TypeScript available.</p>
 
-      <section style={{ display: "flex" }}>
-        <div style={{ borderRight: "1px grey solid", padding: "1rem", flex: 1 }}>
+      <section style={{ display: "flex", flexWrap: "wrap" }}>
+        <div style={{ borderRight: "1px lightgrey solid", padding: "1rem", flex: 1, minWidth: "250px" }}>
           <h3>via NPM</h3>
           <p>You can use npm to install TypeScript globally, this means you can use the <code>tsc</code> command anywhere in your terminal.</p>
           <p>To do this, run <code>npm install -g typescript</code>. This will install the latest version (currently {releaseInfo.tags.stableMajMin}).</p>
           <p>An alternative is to use <a href="https://www.npmjs.com/package/npx">npx</a> when you have to run <code>tsc</code> for one-off occasions.</p>
         </div>
 
-        <div style={{ padding: "1rem", flex: 1 }}>
+        <div style={{ padding: "1rem", flex: 1, minWidth: "250px" }}>
           <h3>via Visual Studio Marketplace</h3>
-          <p>You can install TypeScript as a Visual Studio extension which will allow you to use TypeScript across many MSBuild projects in Visual Studio.</p>
-          <p>The latest version is available <a href={releaseInfo.vs.stable.vs2019_download}>here</a></p>
+          <p>You can install TypeScript as a Visual Studio extension, which will allow you to use TypeScript across many MSBuild projects in Visual Studio.</p>
+          <p>The latest version is available <a href={releaseInfo.vs.stable.vs2019_download}>here</a>.</p>
         </div>
       </section>
-    </div >
+    </div>
 
 
     <div className="raised main-content-block">
       <h2>Working with TypeScript-compatible transpilers</h2>
-      <p>You may want to use a different tool to convert TypeScript files to JavaScript, this could be for speed or consistency existing build tooling.</p>
+      <p>There are other tools which convert TypeScript files to JavaScript files, this could be for speed or consistency with existing build tooling.</p>
       <p>Each of these projects handle the file conversion, but do not handle the type-checking aspects of the TypeScript compiler. So, it's likely you'll still need to keep the TypeScript dependency from above around, and you'll want to enable <Link to="/tsconfig#isolatedModules"><code>isolatedModules</code></Link>.</p>
 
-      <section style={{ display: "flex" }}>
-        <div style={{ borderRight: "1px grey solid", padding: "1rem", flex: 1 }}>
+      <section style={{ display: "flex", flexWrap: "wrap" }}>
+        <div style={{ borderRight: "1px lightgrey solid", padding: "1rem", flex: 1, minWidth: "20px" }}>
           <h3>Babel</h3>
-          <p><a href='https://babeljs.io/'>Babel</a> is a very popular JavaScript transpiler which supports TypeScript files via the plugin <a href='https://babeljs.io/docs/en/babel-preset-typescript#docsNav'>@babel/plugin-transform-typescript</a></p>
+          <p><a href='https://babeljs.io/'>Babel</a> is a very popular JavaScript transpiler which supports TypeScript files via the plugin <a href='https://babeljs.io/docs/en/babel-preset-typescript#docsNav'>@babel/plugin-transform-typescript</a>.</p>
         </div>
 
-        <div style={{ borderRight: "1px grey solid", padding: "1rem", flex: 1 }}>
+        <div style={{ borderRight: "1px lightgrey solid", padding: "1rem", flex: 1, minWidth: "250px" }}>
           <h3>swc</h3>
-          <p><a href='https://swc-project.github.io/docs/installation/'>swc</a> is a fast, rust-based transpiler which supports many of Babel's features which supports TypeScript natively.</p>
+          <p><a href='https://swc-project.github.io/docs/installation/'>swc</a> is a fast, transpiler created in Rust which supports many of Babel's features including TypeScript.</p>
         </div>
 
-        <div style={{ borderRight: "1px grey solid", padding: "1rem", flex: 1 }}>
+        <div style={{ padding: "1rem", flex: 1, minWidth: "200px" }}>
           <h3>Sucrase</h3>
-          <p><a href='https://github.com/alangpierce/sucrase#sucrase/'>Sucrase</a> is a babel fork focused on speed for using in development mode. Sucrose supports TypeScript natively.</p>
+          <p><a href='https://github.com/alangpierce/sucrase#sucrase/'>Sucrase</a> is a Babel fork focused on speed for using in development mode. Sucrose supports TypeScript natively.</p>
         </div>
       </section>
-    </div >
-
-  </Layout >
+    </div>
+  </Layout>
 }
 
 export default (props: any) => <Intl><Index {...props} /></Intl>
