9561c7a713f9bd8d304174321a83c15d9ad47665
Orta Therox <orta.therox@gmail.com>
2020-02-17 20:12:47 -0500

    More links

--- packages/typescriptlang-org/src/redirects/setupRedirects.ts
+++ packages/typescriptlang-org/src/redirects/setupRedirects.ts
@@ -1,17 +1,39 @@
-const { redirects } = require("./oldestRedirects")
+// These are redirects from a very long time ago
+export const veryOldRedirects = {
+  Playground: "/en/play",
+  Tutorial: "docs/home",
+  Handbook: "docs/home",
+  samples: "docs/home/",
+}
+
+// These were .html files in the handbook with some redirection work
+export const handbookRedirects = {
+  "/docs/handbook/writing-declaration-files":
+    "/docs/handbook/declaration-files/introduction.html",
+  "/docs/handbook/writing-definition-files":
+    "/docs/handbook/declaration-files/introduction.html",
+  "/docs/handbook/typings-for-npm-packages":
+    "/docs/handbook/declaration-files/publishing.html",
+
+  "/docs/handbook/release-notes": "/docs/handbook/release-notes/overview",
+}
 
 import { NodePluginArgs } from "gatsby"
 
 export const setupRedirects = (
   createRedirect: NodePluginArgs["actions"]["createRedirect"]
 ) => {
-  const fromArray = Object.keys(redirects)
-  fromArray.forEach(from => {
-    const to = redirects[from]
-
-    createRedirect({
-      fromPath: from,
-      toPath: to,
+  const addRedirects = obj => {
+    const fromArray = Object.keys(obj)
+    fromArray.forEach(from => {
+      const to = obj[from]
+      createRedirect({
+        fromPath: from,
+        toPath: to,
+      })
     })
-  })
+
+    addRedirects(veryOldRedirects)
+    addRedirects(handbookRedirects)
+  }
 }
--- packages/typescriptlang-org/src/templates/pages/download.tsx
+++ packages/typescriptlang-org/src/templates/pages/download.tsx
@@ -3,6 +3,9 @@ import { Layout } from "../../components/layout"
 import { Intl } from "../../components/Intl"
 import { graphql } from "gatsby"
 
+import releaseInfo from "../../lib/release-info.json"
+
+
 const changeExample = (code: string) => document.getElementById("code-example")!.textContent = code
 
 const Index = (props: any) =>
@@ -15,6 +18,7 @@ const Index = (props: any) =>
 
     <div className="raised main-content-block">
       <h1>TypeScript in Your Project</h1>
+      <p>Having TypeScript set up in a per-project basis lets you have many projects with many different versions of TypeScript, this keeps each project working consistently.</p>
       <section style={{ display: "flex" }}>
         <div style={{ borderRight: "1px grey solid", padding: "1rem", flex: 1 }}>
           <h2>via NPM</h2>
@@ -30,7 +34,16 @@ const Index = (props: any) =>
 
         <div style={{ padding: "1rem", flex: 1 }}>
           <h2>via Nuget</h2>
-          <p>You can get TypeScript as a package in Nuget for your MSBuild projects, for example an ASP.NET Core app. You can <a href="https://docs.microsoft.com/en-us/visualstudio/javascript/tutorial-aspnet-with-typescript">install</a> your TypeScript through Visual Studios using via Manage NuGet Packages (which you can get to by right-clicking on a project node) or via the Nuget Package Manager Console (found in Tools > NuGet Package Manager > Package Manager Console) using <code>Install-Package Microsoft.TypeScript.MSBuild</code></p>
+          <p>You can get TypeScript as a package in Nuget for your MSBuild projects, for example an ASP.NET Core app.</p>
+          <p>You can <a href="https://docs.microsoft.com/en-us/visualstudio/javascript/tutorial-aspnet-with-typescript">install</a> TypeScript through Visual Studio using:</p>
+          <ul>
+            <li>
+              The Manage NuGet Packages window (which you can get to by right-clicking on a project node)
+           </li>
+            <li style={{ marginTop: "20px" }}>
+              The Nuget Package Manager Console (found in Tools > NuGet Package Manager > Package Manager Console) using <code>Install-Package Microsoft.TypeScript.MSBuild</code>
+            </li>
+          </ul>
         </div>
       </section>
     </div >
@@ -40,9 +53,8 @@ const Index = (props: any) =>
       <section style={{ display: "flex" }}>
         <div style={{ borderRight: "1px grey solid", padding: "1rem", flex: 1 }}>
           <h2>via NPM</h2>
-          <p>TypeScript is available as a <a href="https://www.npmjs.com/package/typescript">package on the npm registry</a> available as <code>"typescript"</code>.</p>
-          <p>In order to use TypeScript this way, you will need a copy of <a href="https://nodejs.org/en/">Node.js</a> as an environment to run the package. Then you use a dependency manager like <a href='https://www.npmjs.com/'>npm</a>, <a href='https://yarnpkg.com/'>yarn</a> or <a href='https://pnpm.js.org/'>pnpm</a> to download TypeScript into your project.</p>
-          <p>From here, you can use [x, y]</p>
+          <p>{`You can use npm to install TypeScript globally, this means you can use the <code>tsc<code> command anywhere in your terminal. To do this, run <code>npm install -g typescript<code>. This will install the latest version (currently ${releaseInfo.tags.stableMajMin}).`}</p>
+          <p>An alternative is to use <a href="">npx</a>`</p>
         </div>
 
         <div style={{ padding: "1rem", flex: 1 }}>
