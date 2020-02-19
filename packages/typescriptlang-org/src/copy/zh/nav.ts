9d7391e189436075c9f27017b09a02f8c9dd0d10
kingwl <kingwenlu@gmail.com>
2020-02-19 23:17:47 +0800

    add nav and playground translation

--- packages/typescriptlang-org/src/copy/zh/nav.ts
+++ packages/typescriptlang-org/src/copy/zh/nav.ts
@@ -1,19 +1,19 @@
 export const navCopy = {
-  skip_to_content: "Skip to main content",
-  nav_documentation: "Documentation",
-  nav_documentation_short: "Docs",
-  nav_download: "Download",
-  nav_connect: "Connect",
-  nav_playground: "Playground",
-  nav_search_placeholder: "Search Docs",
-  nav_search_aria: "Search the TypeScript site",
-  nav_beta: "Search the TypeScript site",
+  skip_to_content: "跳转到内容",
+  nav_documentation: "文档",
+  nav_documentation_short: "文档",
+  nav_download: "下载",
+  nav_connect: "联系我们",
+  nav_playground: "在线演示",
+  nav_search_placeholder: "搜索文档",
+  nav_search_aria: "搜索 TypeScript 网站",
+  nav_beta: "搜索 TypeScript 网站",
   nav_beta_notification:
-    "Note: this page is a beta page, don't rely on the URL and <a>file issues on microsoft/TypeScript-Website</a>",
+    "注: 这是一个 beta 测试页面, 不要依赖这些 Url，并且 <a>在 microsoft/TypeScript-Website 提出问题</a>",
   // let me know if you can't make this work in your lang:
   // TypeScript X.Y [stable][between?]Z.Y[beta/rc]
-  nav_version_stable_prefix: "is now available",
+  nav_version_stable_prefix: "已经发布",
   nav_version_between: ", ",
-  nav_version_beta_prefix: "is currently in beta.",
-  nav_version_rc_prefix: "has a release candidate to try.",
+  nav_version_beta_prefix: "目前在 beta 测试中。",
+  nav_version_rc_prefix: "可以尝试预发布版本。",
 }
