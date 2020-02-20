9d2c1f64628e6ee03fb4b564f0f37cb3c95e6ecc
kingwl <kingwenlu@gmail.com>
2020-02-21 01:49:20 +0800

    Add mapped type

--- packages/playground-examples/copy/zh/TypeScript/Meta-Types/Mapped Types.ts
+++ packages/playground-examples/copy/zh/TypeScript/Meta-Types/Mapped Types.ts
@@ -1,9 +1,8 @@
-// Mapped types are a way to create new types based
-// on another type. Effectively a transformational type.
+// 映射类型（Mapped types）是通过一个类型创建另一个类型的方法。
+// 从效果上来说，它是一种转换类型。
 
-// Common cases for using a mapped type is dealing with
-// partial subsets of an existing type. For example
-// an API may return an Artist:
+// 常见的映射类型的使用场景是处理某些现有类型的部分子集。
+// 例如，有一个 API 返回 Artist 类型。
 
 interface Artist {
   id: number;
@@ -11,9 +10,8 @@ interface Artist {
   bio: string;
 }
 
-// However, if you were to send an update to the API which
-// only changes a subset of the Artist then you would
-// typically have to create an additional type:
+// 但是，如果要将更新的请求发送给 API，而这个请求只更新 Artist 的
+// 一个子集，则需要创建其他类型：
 
 interface ArtistForEdit {
   id: number;
@@ -21,37 +19,30 @@ interface ArtistForEdit {
   bio?: string;
 }
 
-// It's very likely that this would get out of sync with
-// the Artist above. Mapped types let you create a change
-// in an existing type.
+// 这很可能导致与上面的 Artist 不同步，映射类型允许你根据
+// 已有的类型创建更改的类型。
 
 type MyPartialType<Type> = {
-  // For every existing property inside the type of Type
-  // convert it to be a ?: version
+  // 将 Type 的每个存在的属性转换为可选（?:）的
   [Property in keyof Type]?: Type[Property];
 };
 
-// Now we can use the mapped type instead to create
-// our edit interface:
+// 现在我们可以使用映射类型去创建我们的修改请求接口：
 type MappedArtistForEdit = MyPartialType<Artist>;
 
-// This is close to perfect, but it does allow id to be null
-// which should never happen. So, let's make one quick
-// improvement by using an intersection type (see:
-// example:union-and-intersection-types )
+// 这接近于完美，但是这样做会允许 id 为 null，这是不应该发生的。
+// 所以，让我们使用交集类型（查看 example:union-and-intersection-types）来快速改进。
 
 type MyPartialTypeForEdit<Type> = {
   [Property in keyof Type]?: Type[Property];
 } & { id: number };
 
-// This takes the partial result of the mapped type, and
-// merges it with an object which has id: number set.
-// Effectively forcing id to be in the type.
+// 这将获取映射类型的结果，并且与设置了 { id: number } 的对象类型合并，
+// 有效的在类型中确保了 id 的存在。
 
 type CorrectMappedArtistForEdit = MyPartialTypeForEdit<Artist>;
 
-// This is a pretty simple example of how mapped types
-// work, but covers most of the basics. If you'd like to
-// dive in with more depth, check out the handbook:
+// 这是对映射类型如何工作的一个非常简单的例子，但是涵盖了大多数基础知识。
+// 如果你想更深入的了解更多信息，可以查看手册：
 //
 // https://www.typescriptlang.org/docs/handbook/advanced-types.html#mapped-types
