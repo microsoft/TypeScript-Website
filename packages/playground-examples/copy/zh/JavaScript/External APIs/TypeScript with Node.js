498054e5553a98d2eecd23f066b58e3d51533193
kingwl <kingwenlu@gmail.com>
2020-02-23 16:54:02 +0800

    Add TypeScript with Node

--- packages/playground-examples/copy/zh/JavaScript/External APIs/TypeScript with Node.js
+++ packages/playground-examples/copy/zh/JavaScript/External APIs/TypeScript with Node.js
@@ -1,52 +1,49 @@
 //// { order: 3, isJavaScript: true }
 
-// Node.js is a very popular JavaScript runtime built on v8,
-// the JavaScript engine which powers Chrome. You can use it
-// to build servers, front-end clients and anything in-between.
+// Node.js 是一个非常受欢迎的基于 v8（Chrome 的 JavaScript 引擎）
+// 的 JavaScript 运行时。你可以用它来构建服务器，前端客户端以及他们
+// 之间的任何内容。
 
 // https://nodejs.org/
 
-// Node.js comes with a set of core libraries which extend the
-// JavaScript runtime. They range from path handling:
+// Node.js 带有一组扩展 JavaScript 运行时的核心库，他们包括从路径处理：
 
 import { join } from "path";
 const myPath = join("~", "downloads", "todo_list.json");
 
-// To file manipulation:
+// 到操作文件：
 
 import { readFileSync } from "fs";
 const todoListText = readFileSync(myPath, "utf8");
 
-// You can incrementally add types to your JavaScript projects
-// using JSDoc-style type. We'll make one for our TODO list item
-// based on the JSON structure:
+// 您可以使用 JSDoc 风格的类型来将类型逐步添加到您的 JavaScript 项目中。
+// 我们将基于 JSON 的结构作为列表项构建一个 TODO 列表：
 
 /**
- * @typedef {Object} TODO a TODO item
- * @property {string} title The display name for the TODO item
- * @property {string} body The description of the TODO item
- * @property {boolean} done Whether the TODO item is completed
+ * @typedef {Object} TODO 一个 TODO 元素
+ * @property {string} title TODO 元素所展示的名字
+ * @property {string} body TODO 元素的描述
+ * @property {boolean} done TODO 元素是否已完成
  */
 
-// Now assign that to the return value of JSON.parse
-// to learn more about this, see: example:jsdoc-support
+// 将 JSON.parse 的返回结果赋值给它。
+// 要了解更多关于此的信息，可以查看：example:jsdoc-support
 
-/** @type {TODO[]} a list of TODOs */
+/** @type {TODO[]} TODO 的列表 */
 const todoList = JSON.parse(todoListText);
 
-// And process handling:
+// 以及处理流程：
 import { spawnSync } from "child_process";
 todoList
   .filter(todo => !todo.done)
   .forEach(todo => {
-    // Use the ghi client to create an issue for every todo
-    // list item which hasn't been completed yet.
+    // 使用 ghi 客户端对每个未完成的 todo 列表项创建 issue。
 
-    // Note that you get correct auto-complete and
-    // docs in JS when you highlight 'todo.title' below.
+    // 注意，当您高亮下面的 “todo.title” 时，您会在 JS 中获得
+    // 正确的自动完成功能和文档。
     spawnSync(`ghi open --message "${todo.title}\n${todo.body}"`);
   });
 
-// TypeScript has up-to-date type definitions for all of the
-// built in modules via DefinitelyTyped - which means you
-// can write node programs with strong type coverage.
+// TypeScript 通过 DefinitelyTyped 为所有内置模块提供了最新的
+// 类型定义——这意味着您可以编写具有很高类型覆盖率的 node 程序。
+
