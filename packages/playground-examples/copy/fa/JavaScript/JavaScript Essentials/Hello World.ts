ed71efe0a4ad788b99f71e55958652e48ae64386
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2020-02-23 21:27:05 -0500

    First pass in Farsi

---
+++ packages/playground-examples/copy/fa/JavaScript/JavaScript Essentials/Hello World.ts
@@ -0,0 +1,34 @@
+//// { order: 0, compiler: { target: 1 } }
+
+// به محل خوش آمدید.
+// این ابزار محلی برای آموزش و امتحان Typescript در داخل مرورگر شماست.‍
+
+// The playground makes it easy for you to safely experiment
+// with ideas in TypeScript by making it trivial to share
+// these projects. The URL for this page is everything
+// required to load the project for someone else.
+
+const hello = 'سلام'
+
+// You can see on the right the result of the TypeScript
+// compiler: this is vanilla JavaScript which can run on
+// browsers, servers or anywhere really.
+
+const world = 'دنیا'
+
+// You can see how it makes tiny changes to the code, by
+// converting a "const" to a "var". This is one of the many
+// things TypeScript does to make it possible to run
+// anywhere JavaScript runs.
+
+console.log(hello + ' ' + world)
+
+// Now that you have an idea of how the playground works,
+// let's look at how TypeScript makes working with
+// JavaScript more fun. During this section we'll be trying
+// to keep as close to vanilla JavaScript as possible to
+// show how you can re-use existing knowledge.
+//
+// Click below to continue:
+//
+// example:objects-and-arrays
