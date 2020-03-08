dcda91846bbea42982c0a6e58e4cb138a3a9ee7b
Orta Therox <orta.therox@gmail.com>
2020-03-08 08:16:33 -0400

    Bumped the max limit of handbook subsections to accomodate the adnvacned types page - fixes #323

--- packages/typescriptlang-org/src/templates/handbook.tsx
+++ packages/typescriptlang-org/src/templates/handbook.tsx
@@ -81,7 +81,7 @@ const HandbookTemplate: React.FC<Props> = (props) => {
   if (!post.html) throw new Error(`No html found for the file with props: ${props}`)
 
   const selectedID = post.frontmatter.permalink!.split("/").pop()!.replace(".html", "") || "index"
-  const showSidebar = post.headings && post.headings.length && post.headings.length <= 25
+  const showSidebar = post.headings && post.headings.length && post.headings.length <= 30
 
   return (
     <Layout title={"Handbook - " + post.frontmatter.title} description={post.frontmatter.oneliner || ""} lang="en" allSitePage={props.data.allSitePage}>
