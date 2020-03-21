ac9a39b1c5e2328962b93638f3da3926583c8bac
Orta Therox <orta.therox@gmail.com>
2020-03-21 12:54:58 -0400

    Wrap up the cookie banner:

--- packages/typescriptlang-org/src/components/layout/CookieBanner.tsx
+++ packages/typescriptlang-org/src/components/layout/CookieBanner.tsx
@@ -11,28 +11,36 @@ import { useEffect } from "react"
 
 export const CookieBanner = () => {
   useEffect(() => {
-    const cookieName = "MSCCcc"
+    const cookieName = "MSCC"
 
-    const hasConsent = hasPriorConsent(cookieName)
+    const hasConsent = hasGivenConsentByActivelyUsingTheSite(cookieName)
     if (!hasConsent) {
-      document.getElementById("msccBanner")!.style.display = "block"
-    } else {
+      const banner = document.getElementById("msccBanner")!
+      banner.style.display = "block"
 
+      const giveConsent = () => {
+        const newCookie = document.cookie + ` ;${cookieName}=1`
+        document.cookie = newCookie
+        banner.style.display = "none"
+      }
+
+      document.body.addEventListener('mouseup', giveConsent);
+      document.body.addEventListener('keyup', giveConsent);
+      document.body.addEventListener('submit', giveConsent);
     }
 
-    function hasPriorConsent(cookieName) {
-      var mccRegExp = new RegExp("(?:^|;)\s*" + cookieName + "=[^;]+");
-      return !!document.cookie.match(mccRegExp);
+    function hasGivenConsentByActivelyUsingTheSite(cookieName) {
+      return document.cookie.includes(cookieName)
     }
 
-  })
+  }, [])
 
   return (
-    <div id='msccBanner' dir='ltr' data-site-name='uhf-typescript' data-mscc-version='0.4.2' data-nver='aspnet-3.1.3' data-sver='0.1.2' className='cc-banner' role='alert' aria-labelledby='msccMessage' style={{ display: "none" }}>
+    <div style={{ display: "none" }} id='msccBanner' dir='ltr' data-site-name='uhf-typescript' data-mscc-version='0.4.2' data-nver='aspnet-3.1.3' data-sver='0.1.2' className='cc-banner' role='alert' aria-labelledby='msccMessage' >
       <div className='cc-container'><svg className='cc-icon cc-v-center' x='0px' y='0px' viewBox='0 0 44 44' height='30px' fill='none' stroke='currentColor'>
-        <circle cx='22' cy='22' r='20' stroke-width='2'></circle>
-        <line x1='22' x2='22' y1='18' y2='33' stroke-width='3'></line>
-        <line x1='22' x2='22' y1='12' y2='15' stroke-width='3'></line></svg>
+        <circle cx='22' cy='22' r='20' strokeWidth='2'></circle>
+        <line x1='22' x2='22' y1='18' y2='33' strokeWidth='3'></line>
+        <line x1='22' x2='22' y1='12' y2='15' strokeWidth='3'></line></svg>
         <span id='msccMessage' className='cc-v-center cc-text' tabIndex={0}>This site uses cookies for analytics, personalized content and ads. By continuing to browse this site, you agree to this use.</span> <a href='https://go.microsoft.com/fwlink/?linkid=845480' target='_top' aria-label='Learn more about Microsoft&#39;s Cookie Policy' id='msccLearnMore' className='cc-link cc-v-center cc-float-right' data-mscc-ic='false'>Learn more</a>
       </div>
     </div>
