72f0b7fae457c29cf077701fab17d006209e6b88
Orta Therox <orta.therox@gmail.com>
2020-03-31 20:57:02 -0400

    adds a polyfill for Array.forEach - mght help with #433

--- packages/typescriptlang-org/src/components/layout.tsx
+++ packages/typescriptlang-org/src/components/layout.tsx
@@ -12,6 +12,20 @@ type LayoutProps = SeoProps & Props & {
   children: any
 }
 
+// Test for IE11
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function forEach(callback, thisArg) {
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+    var array = this;
+    thisArg = thisArg || this;
+    for (var i = 0, l = array.length; i !== l; ++i) {
+      callback.call(thisArg, array[i], i, array);
+    }
+  };
+}
+
 export const Layout = (props: LayoutProps) => {
   return (
     <>
