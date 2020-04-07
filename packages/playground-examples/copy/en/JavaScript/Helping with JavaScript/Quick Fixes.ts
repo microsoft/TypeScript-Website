0552683a04959f2930b93d429eb39e731a5afc0a
Jesse Trinity <42591254+jessetrinity@users.noreply.github.com>
2020-04-07 09:33:48 -0700

    Clarify Quick Fixes.ts
    bulb

--- packages/playground-examples/copy/en/JavaScript/Helping with JavaScript/Quick Fixes.ts
+++ packages/playground-examples/copy/en/JavaScript/Helping with JavaScript/Quick Fixes.ts
@@ -1,13 +1,13 @@
 // TypeScript provides quick-fix recommendations for
-// common accidents, they show up in your editor based
-// on recommendations.
+// common accidents. Prompts show up in your editor based
+// on these recommendations.
 
 // For example TypeScript can provide quick-fixes
 // for typos in your types:
 
 const eulersNumber = 2.7182818284;
 eulersNumber.toStrang();
-//           ^______^ - select this to see the light build
+//           ^______^ - select this to see the light bulb
 
 class ExampleClass {
   method() {
