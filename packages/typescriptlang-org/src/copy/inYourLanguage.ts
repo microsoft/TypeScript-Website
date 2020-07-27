11136c5d5739d9d6fdc2c7ec3b8c6e1a412255e5
bob1983 <bob.1983.g@gmail.com>
2020-07-27 15:14:13 +0900

    Add inYourLangage ja translation

--- packages/typescriptlang-org/src/copy/inYourLanguage.ts
+++ packages/typescriptlang-org/src/copy/inYourLanguage.ts
@@ -8,4 +8,10 @@ export const inYourLanguage: Record<
     open: "Go",
     cancel: "Don't ask again",
   },
+  ja: {
+    shorthand: "日本語",
+    body: "このページを日本語で利用しますか？",
+    open: "利用する",
+    cancel: "質問を表示しない"
+  },
 }
