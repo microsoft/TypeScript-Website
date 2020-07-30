0b35a227aa5772fcaab99548c96f4411401a72e5
Danilo Fuchs <danilo_fuchs@hotmail.com>
2020-07-30 16:29:26 -0300

    fix: Add missing pt.ts messages definition file

--- packages/typescriptlang-org/src/copy/pt.ts
+++ packages/typescriptlang-org/src/copy/pt.ts
@@ -1,8 +1,10 @@
 import { defineMessages } from "react-intl"
 import { Copy, messages as englishMessages } from "./en"
 import { docCopy } from "./pt/documentation"
+import { comCopy } from "./pt/community"
 
 export const lang: Copy = defineMessages({
   ...englishMessages,
   ...docCopy,
+  ...comCopy,
 })
