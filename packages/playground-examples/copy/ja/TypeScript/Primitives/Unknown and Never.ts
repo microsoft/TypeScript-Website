8050daf2b77b39850610bfa03e962b4dc42935b6
bob1983 <bob.1983.g@gmail.com>
2020-08-01 23:14:06 +0900

    fix translation

--- packages/playground-examples/copy/ja/TypeScript/Primitives/Unknown and Never.ts
+++ packages/playground-examples/copy/ja/TypeScript/Primitives/Unknown and Never.ts
@@ -1,14 +1,14 @@
 // unknown型
 
-// unknown型は、非常に多くの箇所で使用されています。
+// unknown型は、一度詳しくなると、非常に多くの用途があることに気が付きます。
 // unknown型は、any型とよく似た振る舞いをします。
-// 違いは、anyは曖昧なままでも使用できますが、 unknownは
-// 詳細を必要とする点です。
+// anyは曖昧なままでも使用できますが、 unknownは
+// 特定を必要とします。
 
 // JSONパーサーのWrapperは良い例です。
-// JSONのデータは、多種多様であるため、JSONパーサーの作者は、
+// JSONのデータは、多種多様であるため、JSONパーサーの利用者が
+// データの形を知っているにもかかわらず、JSONパーサーの作者は、
 // データがどのような形になっているのかを知ることはできません。
-// データの形は、パーサーを呼び出す利用者が知っているべきなのです。
 
 const jsonParser = (jsonString: string) => JSON.parse(jsonString);
 
@@ -19,8 +19,8 @@ myAccount.email;
 
 // 変数jsonParserにカーソルを合わせると、戻り値の型がany型であることがわかります。
 // そして、変数myAccountも同様にany型になります。
-// こちらについて、ジェネリクスを使って型付けすることもできますが、
-// unknown型を使うことによっても修正できます。
+// これについて、ジェネリクスを使って型付けすることもできますが、
+// unknown型を使うことによっても型付けきます。
 
 const jsonParserUnknown = (jsonString: string): unknown => JSON.parse(jsonString);
 
@@ -28,7 +28,7 @@ const myOtherAccount = jsonParserUnknown(`{ "name": "Samuel" }`);
 
 myOtherAccount.name;
 
-// myOtherAccountの型をTypeScriptに示すまでは、myOtherAccountのプロパティを使用する
+// myOtherAccountの型をTypeScriptに示すまでは、myOtherAccountのオブジェクトを使用する
 // ことができません。こうすることによって、APIの使用者に、型について前もって
 // 考えさせることができます。
 
@@ -36,15 +36,14 @@ type User = { name: string };
 const myUserAccount = jsonParserUnknown(`{ "name": "Samuel" }`) as User;
 myUserAccount.name;
 
-// unknown型はとても優れたツールです。
-// unknown型をより深く理解したい場合はこれらを参照してください。
+// unknown型はとても優れたツールです。より深く理解したい場合はこれらを参照してください。
 // https://mariusschulz.com/blog/the-unknown-type-in-typescript
 // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-0.html#new-unknown-top-type
 
 // never型
 
 // TypeScriptは、データフローの解析をサポートしているため、
-// 論理的に絶対に発生しないコードを表現することができる必要があります。
+// 論理的に絶対に発生しないコードを表現できる必要があります。
 // 例えば、こちらの関数は絶対にreturnされません。
 
 const neverReturns = () => {
@@ -54,25 +53,27 @@ const neverReturns = () => {
 
 // この関数にカーソルを合わせると、型が () => neverとなっていることがわかります。
 // つまり、この関数は絶対にreturnされません。
-// neverは、他の値と同様に、代入することは可能です。
+// これらは、他の値と同様に、代入することは可能です。
 
 const myValue = neverReturns();
 
-// never型を返す関数は、JavaScriptランタイムの予想できない挙動に
-// 対応する場合や、APIの利用者が、型を使わない可能性がある場合に有用です。
+// never型を返す関数は、
+// JavaScriptランタイムの予想できない挙動に対応する場合や、
+// APIの利用者が型を使わない可能性がある場合に有用です。
 
 const validateUser = (user: User) => {
   if (user) {
     return user.name !== "NaN";
   }
   // 型システム的には、このコードは絶対に実行されません。
-  // つまり、関数neverReturnsの戻り値の型(never型)に合致します。
+  // つまり、関数neverReturnsの戻り値の型に合致します。
 
   return neverReturns();
 };
 
 // この関数の型定義では、引数userが必ず渡されることになっていますが、
-// JavaScriptには様々な抜け道があるので、それを保証することはできません。
+// JavaScriptには様々な抜け道があるので、
+// それを保証することはできません。
 
 // 上記の例のようにnever型を返す関数を使うことで、実行される可能性がない箇所に
 // コードを追加することができます。
@@ -80,7 +81,7 @@ const validateUser = (user: User) => {
 // ループなどのリソースをクローズしたい場合などで有用です。
 
 // とてもよくあるnever型の使われ方として、
-// switch文でのすべてのパスが網羅されていることのチェックがあります。
+// switch文でのすべてのパスが網羅されていることの担保があります。
 
 // 以下は、enumと、neverによってすべてのパスが網羅されているswith文の例です。
 // 試しにFlowerに新しいオプションを追加してみてください(例: Tulipなど)
@@ -114,11 +115,13 @@ const flowerLatinName = (flower: Flower) => {
 
 // 共用体でのnever型
 
-// 共用体では、never型は自動的に取り除かれます。
+// 共用体では、never型は
+// 自動的に取り除かれます。
 
 type NeverIsRemoved = string | never | number;
 
-// NeverIsRemovedの型をみてみると、string | number になっています。
+// NeverIsRemovedの型を見てみると、
+// string | number になっています。
 // これは、never型の代入は実行時には絶対に起こらないためです。
 
 // この特徴は、Conditinal Typesで良く使われています。 example:conditional-types
