320085dd4338ea181c7bbe8192fc49e056201b87
stkevintan <stkevintan@zju.edu.cn>
2020-08-04 20:53:03 +0800

    feat: update translations

--- packages/typescriptlang-org/src/copy/zh/community.ts
+++ packages/typescriptlang-org/src/copy/zh/community.ts
@@ -11,9 +11,9 @@ export const comCopy = {
   com_online_discord_header: "交流",
   com_online_discord_desc:
     "在 TypeScript 社区聊天室中与其他 TypeScript 用户聊天。",
-  com_online_github_desc: "找到了 bug ，或者有你独到的见解？",
+  com_online_github_desc: "找到了 bug，或者有你独到的见解？",
   com_online_github_href: "在 GitHub 上告诉我们",
-  com_online_twitter_desc: "保持最新的消息。在 Twitter 上关注我们",
+  com_online_twitter_desc: "跟进最新进展。在 Twitter 上关注我们",
   com_online_blog_desc: "了解最新的 Typescript 的进展，通过我们的",
   com_online_typed_desc: "TypeScript 定义文件",
   com_online_typed_href: "浏览数千个",
