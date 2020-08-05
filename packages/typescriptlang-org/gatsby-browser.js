8106759b9b4c430218c0fd38309d01a25595e4cd
François Chalifour <francois.chalifour@gmail.com>
2020-08-05 13:21:18 +0200

    Implement DocSearch v3
    Co-authored-by: Shipow <shipowlata@gmail.com>

---
+++ packages/typescriptlang-org/gatsby-browser.js
@@ -0,0 +1,6 @@
+require("@docsearch/react/style/variables")
+require("@docsearch/react/style/button")
+
+// A stub function is needed because Gatsby won't load this file otherwise
+// (https://github.com/gatsbyjs/gatsby/issues/6759)
+exports.onClientEntry = () => {}
