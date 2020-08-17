39dbcc31272721023f2e89ab65300974958db0dd
Danilo Fuchs <danilo_fuchs@hotmail.com>
2020-08-17 19:10:28 -0300

    fix: Add missing PT copy entries

--- packages/typescriptlang-org/src/copy/pt.ts
+++ packages/typescriptlang-org/src/copy/pt.ts
@@ -1,10 +1,16 @@
 import { defineMessages } from "react-intl"
 import { Copy, messages as englishMessages } from "./en"
+import { navCopy } from "./pt/nav"
 import { docCopy } from "./pt/documentation"
+import { indexCopy } from "./pt/index"
 import { comCopy } from "./pt/community"
+import { handbookCopy } from "./pt/handbook"
 
 export const lang: Copy = defineMessages({
   ...englishMessages,
+  ...navCopy,
   ...docCopy,
+  ...indexCopy,
   ...comCopy,
+  ...handbookCopy,
 })
