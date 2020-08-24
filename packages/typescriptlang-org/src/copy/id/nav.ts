d0638b288aba981a92259d2a6ff47ec0d1d66908
trastanechora <trastanechora@gmail.com>
2020-08-24 20:16:53 +0700

    Added nav translation for Bahasa Indonesia

---
+++ packages/typescriptlang-org/src/copy/id/nav.ts
@@ -0,0 +1,22 @@
+export const navCopy = {
+  skip_to_content: "Langsung ke konten utama",
+  nav_documentation: "Dokumentasi",
+  nav_documentation_short: "Dokumentasi",
+  nav_download: "Unduh",
+  nav_community: "Komunitas",
+  nav_playground: "Coba",
+  nav_playground_short: "Coba",
+  nav_handbook: "Petunjuk",
+  nav_tools: "Alat",
+  nav_search_placeholder: "Cari Dokumentasi",
+  nav_search_aria: "Cari di situs TypeScript",
+  // let me know if you can't make this work in your lang:
+  // TypeScript X.Y [stable][between?]Z.Y[beta/rc]
+  nav_version_stable_prefix: "telah tersedia sekarang",
+  nav_version_between: ", ",
+  nav_version_beta_prefix: "saat ini dalam versi beta.",
+  nav_version_rc_prefix: "memiliki kandidat rilis untuk dicoba.",
+  nav_this_page_in_your_lang: "Halaman ini telah tersedia di bahasa Anda",
+  nav_this_page_in_your_lang_open: "Buka",
+  nav_this_page_in_your_lang_no_more: "Jangan munculkan kembali",
+}
