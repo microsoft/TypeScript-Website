1385be5f3cf4daa8adaf0688be71a0e12d7626ba
trastanechora <trastanechora@gmail.com>
2020-08-24 20:17:18 +0700

    Register navCopy into root Bahasa Indonesia site copy

--- packages/typescriptlang-org/src/copy/id.ts
+++ packages/typescriptlang-org/src/copy/id.ts
@@ -1,10 +1,12 @@
 import { defineMessages } from "react-intl"
 import { comCopy } from "./id/community"
 import { docCopy } from "./id/documentation"
+import { navCopy } from "./id/nav"
 import { Copy, messages as englishMessages } from "./en"
 
 export const lang: Copy = defineMessages({
   ...englishMessages,
   ...comCopy,
   ...docCopy,
+  ...navCopy,
 })
