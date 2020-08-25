4fd05c2c24c07ff749333203b2b6f64d00354bf7
Lucas Picollo <lucas.picollo@geekhunter.com.br>
2020-08-25 10:20:53 -0300

    fix: Types vs Interface.ts code translated

--- packages/playground-examples/copy/pt/Typescript/Language Extensions/Types vs Interfaces.ts
+++ packages/playground-examples/copy/pt/Typescript/Language Extensions/Types vs Interfaces.ts
@@ -4,71 +4,71 @@
 // Eles são bem parecidos e para a maioria dos casos
 // funcionam da mesma forma.
 
-type BirdType = {
-  wings: 2;
+type TipoPassaro = {
+  asas: 2;
 };
 
-interface BirdInterface {
-  wings: 2;
+interface InterfacePassaro {
+  asas: 2;
 }
 
-const bird1: BirdType = { wings: 2 };
-const bird2: BirdInterface = { wings: 2 };
+const passaro1: TipoPassaro = { asas: 2 };
+const passaro2: InterfacePassaro = { asas: 2 };
 
 // Porque o Typescript é um sistema de tipagem estrutural
 // é possível misturar o seu uso também.
 
-const bird3: BirdInterface = bird1;
+const passaro3: InterfacePassaro = passaro1;
 
 // Ambos suportam a extensão de outras interfaces e tipos.
 // Os tipos fazem isso através da interseção de tipos,
 // enquanto interfaces possuem uma palavra-chave.
 
-type Owl = { nocturnal: true } & BirdType;
-type Robin = { nocturnal: false } & BirdInterface;
+type Coruja = { noturno: true } & TipoPassaro;
+type Robin = { noturno: false } & InterfacePassaro;
 
-interface Peacock extends BirdType {
-  colourful: true;
-  flies: false;
+interface Pavao extends TipoPassaro {
+  colorido: true;
+  voa: false;
 }
-interface Chicken extends BirdInterface {
-  colourful: false;
-  flies: false;
+interface Galinha extends InterfacePassaro {
+  colorido: false;
+  voa: false;
 }
 
-let owl: Owl = { wings: 2, nocturnal: true };
-let chicken: Chicken = { wings: 2, colourful: false, flies: false };
+let coruja: Coruja = { asas: 2, noturno: true };
+let galinha: Galinha = { asas: 2, colorido: false, voa: false };
 
 // Tendo dito isso nós recomendamos você a usar interfaces ao invés de tipos.
 // Especialmente porque você recebe melhores mensagens de erro.
 // Se passar o mouse sobre o erro, você pode ver os erros que o Typescript
-// mais focados para interfaces como a Chicken.
+// mais focados para interfaces como a Galinha.
 
-owl = chicken;
-chicken = owl;
+coruja = galinha;
+galinha = coruja;
 
 // Uma das maiores diferenças entre tipos e interfaces é que
 // interfaces são abertas e tipos são fechados.
 // Isso signifca que você pode extender interfaces declarando
 // uma segunda vez.
 
-interface Kitten {
-  purrs: boolean;
+interface Gato {
+  ronrona: boolean;
 }
 
-interface Kitten {
-  colour: string;
+interface Gato {
+  cor: string;
 }
 
 // Por outro lado tipos não podem ser alterados fora
 // da prórpia declaração.
 
-type Puppy = {
-  color: string;
+type Filhote = {
+  cor: string;
 };
 
-type Puppy = {
-  toys: number;
+type Filhote = {
+  brinquedos: number;
 };
 
 // Dependendo dos seus objetivos essa diferença pode ser
