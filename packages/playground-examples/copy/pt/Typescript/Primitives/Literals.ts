04eb8d00efa4570e333617c99fa5707e3c1612d0
Lucas Picollo <lucas.picollo@geekhunter.com.br>
2020-08-25 10:21:00 -0300

    fix: Literals.ts code translated

--- packages/playground-examples/copy/pt/Typescript/Primitives/Literals.ts
+++ packages/playground-examples/copy/pt/Typescript/Primitives/Literals.ts
@@ -8,57 +8,57 @@
 // Isso significa que "Olá Mundo" é uma string, mas uma string
 // não é um "Olá Mundo" dentro do sistema de tipos.
 
-const helloWorld = "Hello World";
-let hiWorld = "Hi World"; // isso é uma string porque é um let
+const olaMundo = "Olá Mundo";
+let oiMundo = "Oi Mundo"; // isso é uma string porque foi declarado com let
 
 // Essa função recebe qualquer string
-declare function allowsAnyString(arg: string);
-allowsAnyString(helloWorld);
-allowsAnyString(hiWorld);
+declare function permiteQualquerString(arg: string);
+permiteQualquerString(olaMundo);
+permiteQualquerString(oiMundo);
 
-// Essa função aceita apenas o literal "Hello World"
-declare function allowsOnlyHello(arg: "Hello World");
-allowsOnlyHello(helloWorld);
-allowsOnlyHello(hiWorld);
+// Essa função aceita apenas o literal "Olá Mundo"
+declare function permiteApenasOlaMundo(arg: "Olá Mundo");
+permiteApenasOlaMundo(olaMundo);
+permiteApenasOlaMundo(oiMundo);
 
 // Isso permite você declarar APIs que usam unions para dizer
 // que só aceitam literais específicos:
 
-declare function allowsFirstFiveNumbers(arg: 1 | 2 | 3 | 4 | 5);
-allowsFirstFiveNumbers(1);
-allowsFirstFiveNumbers(10);
+declare function permiteOsPrimeirosCincoNumeros(arg: 1 | 2 | 3 | 4 | 5);
+permiteOsPrimeirosCincoNumeros(1);
+permiteOsPrimeirosCincoNumeros(10);
 
-let potentiallyAnyNumber = 3;
-allowsFirstFiveNumbers(potentiallyAnyNumber);
+let potencilamenteQualquerNumero = 3;
+permiteOsPrimeirosCincoNumeros(potencilamenteQualquerNumero);
 
 // A primeira vista essa regra não é aplicada para objetos complexos.
 
-const myUser = {
-  name: "Sabrina",
+const meuUsuario = {
+  nome: "Sabrina",
 };
 
-// Veja como isso transforma `name: "Sabrina"` para `name: string`
+// Veja como isso transforma `nome: "Sabrina"` para `nome: string`
 // mesmo que definido como uma constante. Isso acontece pelo fato do nome
 // poder ser alterado a qualquer momento:
 
-myUser.name = "Cynthia";
+meuUsuario.nome = "Cynthia";
 
-// Porque o nome do objeto myUser poder ser alterado, o TypeScript
+// Porque o nome do objeto meuUsuario poder ser alterado, o TypeScript
 // não pode usar a versão literal no sistema de tipagem.
 // No entanto existe uma funcionalidade que permite você fazer isso.
 
-const myUnchangingUser = {
-  name: "Fatma",
+const meuUsuarioImutavel = {
+  nome: "Fatma",
 } as const;
 
 // Quado "as const" é aplicado ao objeto, ele se torna
 // um objeto literal que não pode ser alterado ao invés de um
 // objeto mutável.
 
-myUnchangingUser.name = "Raîssa";
+meuUsuarioImutavel.nome = "Raîssa";
 
 // "as const" é uma ótima ferramenta para dados fixos e lugares
 // onde você trata código como literal. "as const" também
 // funciona com arrays:
 
-const exampleUsers = [{ name: "Brian" }, { name: "Fahrooq" }] as const;
+const usuariosExemplos = [{ nome: "Brian" }, { nome: "Fahrooq" }] as const;
