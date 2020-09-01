ac8db796cbf6ae645d2e3d3b84d1e1ed1589ffb2
Orta <git@orta.io>
2020-09-01 10:37:28 -0400

    Dangerfile faff

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,39 +2,60 @@
 // yarn danger pr https://github.com/microsoft/TypeScript-Website/pull/115
 
 import { danger, message, markdown } from "danger"
-import { basename } from "path"
-import spellcheck from "danger-plugin-spellcheck"
+// import { basename } from "path"
+// import spellcheck from "danger-plugin-spellcheck"
 import lighthouse from "danger-plugin-lighthouse"
 
 // Spell check all the things
-spellcheck({ settings: "artsy/peril-settings@spellcheck.json" })
+// spellcheck({ settings: "artsy/peril-settings@spellcheck.json" })
 
-// Print out the PR url
-const deployURL = process.env.PR_DEPLOY_URL_ROOT
-message(
-  `Deployed to [a PR branch](${deployURL}) - [playground](${deployURL}/play) [tsconfig](${deployURL}/tsconfig) [old handbook](${deployURL}/docs/handbook/integrating-with-build-tools.html)`
-)
+export default (webhook: any) => {
+  console.log(webhook)
 
-// Look for new snapshots and show in a HTML table
-const snapshots = danger.git.fileMatch("packages/typescriptlang-org/_tests/backstop_data/bitmaps_reference/*.png")
-if (snapshots.modified) {
-  const oldSha = danger.github.pr.base.sha
-  const newSha = danger.github.pr.head.sha
+  console.log(process.env.PR_DEPLOY_URL_ROOT)
 
-  const tables = snapshots.getKeyedPaths().modified.map(p => {
-    const oldURL = `https://raw.githubusercontent.com/microsoft/TypeScript-Website/${oldSha}/${p}`
-    const newURL = `https://raw.githubusercontent.com/microsoft/TypeScript-Website/${newSha}/${p}`
+  const changedFiles = getChangedFiles(webhook.pull_request.number)
+  console.log(changedFiles)
 
-    return `
-###### \`${basename(p)}\`
+  // Print out the PR url
+  const deployURL = process.env.PR_DEPLOY_URL_ROOT
+  message(
+    `Deployed to [a PR branch](${deployURL}) - [playground](${deployURL}/play) [tsconfig](${deployURL}/tsconfig) [old handbook](${deployURL}/docs/handbook/integrating-with-build-tools.html)`
+  )
 
-Before             |  After
-:-------------------------:|:-------------------------:
-![](${oldURL})  |  ![](${newURL})
-`
-  })
-
-  markdown(`## Snapshots updated\n\n ${tables.join("\n\n")}`)
+  lighthouse()
 }
 
-lighthouse()
+const getChangedFiles = async (prNumber: number) => {
+  const repo = { owner: "microsoft", name: "TypeScript-website" }
+
+  // https://developer.github.com/v3/pulls/#list-pull-requests-files
+  const options = danger.github.api.pulls.listFiles.endpoint.merge({ ...repo, pull_number: prNumber })
+
+  /** @type { import("@octokit/rest").PullsListFilesResponseItem[]} */
+  const files = await danger.github.api.paginate(options)
+  const fileStrings = files.map(f => `/${f.filename}`)
+  return fileStrings
+}
+
+// // Look for new snapshots and show in a HTML table
+// const snapshots = danger.git.fileMatch("packages/typescriptlang-org/_tests/backstop_data/bitmaps_reference/*.png")
+// if (snapshots.modified) {
+//   const oldSha = danger.github.pr.base.sha
+//   const newSha = danger.github.pr.head.sha
+
+//   const tables = snapshots.getKeyedPaths().modified.map(p => {
+//     const oldURL = `https://raw.githubusercontent.com/microsoft/TypeScript-Website/${oldSha}/${p}`
+//     const newURL = `https://raw.githubusercontent.com/microsoft/TypeScript-Website/${newSha}/${p}`
+
+//     return `
+// ###### \`${basename(p)}\`
+
+// Before             |  After
+// :-------------------------:|:-------------------------:
+// ![](${oldURL})  |  ![](${newURL})
+// `
+//   })
+
+//   markdown(`## Snapshots updated\n\n ${tables.join("\n\n")}`)
+// }
