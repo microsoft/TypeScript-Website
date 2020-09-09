2cc41204fbcfa593090d1d577fa08e82b30b5b24
Roger Oliveira <rogerpolvr@gmail.com>
2020-09-08 21:14:26 -0300

    Translating Mobile Support.ts to PT-BR

---
+++ packages/playground-examples/copy/pt/Playground/Tooling/Mobile Support.ts
@@ -0,0 +1,15 @@
+//// { order: 99 }
+
+// Dispositivos pequenos podem não ser muito bons para escrever código,
+// mas são bons para ler nossos exemplos ou fornecer revisões rápidas
+// para alguém e solicitar pequenas alterações.
+
+// Com o novo Playground, fizemos algumas melhorias na
+// experiência para telas pequenas, como:
+
+//  - Removemos itens de menu que não estão relacionados ao código.
+//  - Pequenas melhorias no visual.
+//  - Mudamos para um painel único no editor. Isso significa
+//      que você não pode mais ver o JS transpilado.
+
+// Isso deve tornar a experiência menos dolorosa.
