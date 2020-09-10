04c9c8ddada56fe3db29356bc4604a92aed39e04
luan.sabino <39681078+luan-sabino@users.noreply.github.com>
2020-09-09 21:46:14 -0300

    Translated "Private Class Fields.ts" to ptbr

--- packages/playground-examples/copy/pt/3-8/Syntax and Messaging/Private Class Fields.ts
+++ packages/playground-examples/copy/pt/3-8/Syntax and Messaging/Private Class Fields.ts
@@ -1,69 +1,70 @@
 //// { compiler: { ts: "3.8.3" } }
-// 3.8 adds private fields, which are a way of declaring a class field to
-// be unavailable outside of the containing class, including to subclasses.
+// A versão 3.8 adicionou os campos privados, que é uma maneira de declarar
+// que um campo de uma classe é inacessível fora daquela classe, incluindo subclasses.
 
-// For example, the Person class below does not allow for anyone using an
-// instance of the class to read  the firstName, lastName or prefix
+// Por exemplo, a classe Pessoa abaixo não permite que ninguém usando uma
+// instância da classe leia o primeiroNome, sobrenome, e o prefixo.
 
-class Person {
-  #firstName: string;
-  #lastName: string;
-  #prefix: string;
-
-  constructor(firstName: string, lastName: string, prefix: string) {
-    this.#firstName = firstName;
-    this.#lastName = lastName;
-    this.#prefix = prefix;
+class Pessoa {
+  #primeiroNome: string;
+  #sobrenome: string;
+  #prefixo: string;
+  
+  constructor(primeiroNome: string, sobrenome: string, prefixo:string) {
+    this.#primeiroNome = primeiroNome;
+    this.#sobrenome = sobrenome;
+    this.#prefixo = prefixo;
   }
 
-  greet() {
-    // In iceland it is preferable to use a full name instead of [prefix] [lastname]
+  cumprimentar() {
+    // Na Islândia é preferível que usemos o nome completo ao invés de [prefixo] [sobrenome]
     // https://www.w3.org/International/questions/qa-personal-names#patronymic
-    if (navigator.languages[0] === "is") {
-      console.log(`Góðan dag, ${this.#firstName} ${this.#lastName}`);
+    if(navigator.languages[0] === "is") {
+      console.log(`Góðan dag, ${this.#primeiroNome} ${this.#sobrenome}`);
     } else {
-      console.log(`Hello, ${this.#prefix} ${this.#lastName}`);
+      console.log(`Olá, ${this.#prefixo} ${this.#sobrenome}`);
     }
   }
 }
 
-let jeremy = new Person("Jeremy", "Bearimy", "Mr");
+let jeremias = new Pessoa("Jeremias", "Beremias", "Sr.");
+     
+// Você não pode acessar nenhum dos campos dessa classe de fora dela:
 
-// You can't get to any of the private fields from outside that class:
+// Por exemplo, isso não funciona:
+console.log(jeremias.#primeiroNome);
 
-// For example, this won't work:
-console.log(jeremy.#lastName);
+// Nem isso:
+console.log("Sobrenome de Pessoas:", jeremy["#sobrenome"]);
 
-// Nor this:
-console.log("Person's last name:", jeremy["#lastName"]);
+// Uma pergunta comum que fazemos é "Porque você usaria isso no lugar da
+// palavra-chave 'private' dentro de uma classe?" - vamos fazer uma 
+// comparação sobre para ver como isso funciona apos a 3.8:
 
-// A common question we get is "Why would you use this over the 'private'
-// keyword in a classfield?" - let's look by making a comparison to
-// how it worked in TypeScript before 3.8:
-
-class Dog {
-  private _name: string;
-  constructor(name: string) {
+class cachorro {
+  private _nome: string;
+  constructor(nome: string) {
     this._name = name;
   }
 }
 
-let oby = new Dog("Oby");
-// Won't let you access via dot notation
-oby._name = "Spot";
-// But TypeScript allows bracket notation as an escape clause
-oby["_name"] = "Cherny";
+let rex = new cachorro("Rex");
+// Não permite que você acesse através da notação de ponto
+rex._nome = "Caramelo";
+// Mas permite que seja acessado através da notação de colchetes.
+rex["_nome"] = "Bidu";
 
-// The TypeScript reference of private only exists at type-level
-// which means that you can only trust it so far. With private fields
-// soon to be a part of the JavaScript language, then you can make better
-// guarantees about the visibility of your code.
+// A referencia de private no TypeScript existe no type-level o que
+// significa que você só pode confiar nisso. Agora com os campos
+// privados fazendo parte da linguagem JavaScript, você pode então
+// garantir de uma forma melhor a visibilidade no seu código.
 
-// We don't plan on deprecating the `private` field keyword
-// in TypeScript, so your existing code will continue to work, but now
-// you can write code which is closer to the JavaScript language instead.
+// Nós não planejamos descontinuar a palavra-chave `private` 
+// no TypeScript, então os seus códigos ja existentes vão continuar
+// a funcionar, mas agora você pode escrever código de uma forma mais
+// proxima da linguagem JavaScript.
 
-// You can learn more about class fields in the tc39 proposal
+// Você pode aprender mais sobre campos de classes na tc39 proposta
 // https://github.com/tc39/proposal-class-fields/
-// and the beta release notes:
+// e nas notas de lançamentos beta:
 // https://devblogs.microsoft.com/typescript/announcing-typescript-3-8-beta/#ecmascript-private-fields
