a3a644327337032ac76bedbe3377d10b6715a58e
AzureAD\신지원 <cider134@gmail.com>
2020-09-18 20:45:59 +0900

    modify ko translation for Big number literals.ts

--- packages/playground-examples/copy/ko/3-7/Fixits/Big number literals.ts
+++ packages/playground-examples/copy/ko/3-7/Fixits/Big number literals.ts
@@ -1,12 +1,12 @@
 //// { compiler: {  target: 99 }, order: 1 }
 
-// JavaScript를 쓰는 동안 JavaScript에서 표현할 수 있는 숫자의 크기에 
+// JavaScript에서 표현할 수 있는 숫자의 크기에 
 // 제한이 있다는 걸 알고 계셨나요?
 
 const maxHighValue = 9007199254740991;
 const maxLowValue = -9007199254740991;
 
-// 이 숫자보다 수가 하나라도 더 크거나 작으면
+// 아래 숫자보다 수가 하나라도 더 크거나 작으면
 // 매우 위험해집니다
 
 const oneOverMax = 9007199254740992;
@@ -18,10 +18,10 @@ const oneBelowMin = -9007199254740992;
 //
 // https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/BigInt
 
-// TypeScript는 이제 JavaScript에 유형이
-// BigInt 여야 한다는 것을 알리는 접미사 "n"을 추가하는
+// TypeScript는 이제 JavaScript에 타입이
+// BigInt 여야 한다는 것을 알리는 접미사 "n"을 추가하여
 // 2^52(양수/음수) 이상의 숫자 리터럴에 대한
-// 숫자 리터럴을 위한 기능을 제공할 것입니다.
+// 기능을 제공합니다.
 
 // 숫자 리터럴
 9007199254740993;
@@ -29,7 +29,7 @@ const oneBelowMin = -9007199254740992;
 9007199254740994;
 -9007199254740994;
 
-// 6진수
+// 16진수
 0x19999999999999;
 -0x19999999999999;
 0x20000000000000;
