3594efde542b81a813f28471d5256694c5ac44a3
Orta <git@orta.io>
2020-09-27 09:42:09 -0400

    Adds the ukrainian intl issue

--- packages/typescriptlang-org/scripts/makeMarkdownOfTranslations.js
+++ packages/typescriptlang-org/scripts/makeMarkdownOfTranslations.js
@@ -54,8 +54,11 @@ const getAllTODOFiles = lang => {
   return all
 }
 
+let totalDone = 0
+let totalTodo = 0
+
 const toMarkdown = files => {
-  const md = []
+  const md = [""]
 
   const markdownLink = (f, done) => {
     const name = basename(f)
@@ -69,8 +72,8 @@ const toMarkdown = files => {
     const todo = files[section].todo
     const done = files[section].done
 
-    md.push("\n\n## " + section + "\n\n")
-
+    md.push("\n\n## " + section + "\n")
+    md.push(`Done: ${done.length}, TODO: ${todo.length}.\n\n`)
     done.forEach(f => {
       md.push(markdownLink(f, true))
     })
@@ -78,8 +81,12 @@ const toMarkdown = files => {
     todo.forEach(f => {
       md.push(markdownLink(f))
     })
+
+    totalDone += done.length
+    totalTodo += todo.length
   })
 
+  md[0] = `For this language there are ${totalDone} translated files, with ${totalTodo} TODO.\n\n`
   return md.join("\n")
 }
 
