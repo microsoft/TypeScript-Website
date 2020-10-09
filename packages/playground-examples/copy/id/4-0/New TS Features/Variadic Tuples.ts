65ada78de6bef011240b32551d7d22e86f61512e
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-10-09 16:42:05 +0700

    docs(i18n-id): Provide Indonesian translation for variadic tuples

--- packages/playground-examples/copy/id/4-0/New TS Features/Variadic Tuples.ts
+++ packages/playground-examples/copy/id/4-0/New TS Features/Variadic Tuples.ts
@@ -1,47 +1,58 @@
 //// { compiler: { ts: "4.0.2" } }
-// Variadic Tuples gives tuples the ability to handle the rest operator (...)
-// to pass types through type checker in a way that works like generics.
 
-// This is quite an advanced topic, so if you get lost do not worry too much.
-// It builds on example:generic-functions and example:tuples
+// _Variadic tuples_ memberikan kemampuan pada _tuple_ untuk menangani
+// operator _rest_ (...) untuk memberikan tipe pada pemeriksa tipe
+// dengan cara yang mirip dengan tipe generik.
 
-// To start off, here is a variadic tuple which will always prefix another
-// tuple with a number:
+// _Variadic tuples_ merupakan topik yang rumit, sehingga Anda tidak perlu
+// khawatir bila Anda merasa kebingungan. Contoh di bawah berdasarkan
+// example:generic-functions dan example:tuples.
 
-type AddMax<T extends unknown[]> = [max: number,  ...rest: T];
-//          ^ Generic used to constrain the T
-//                                                ^ ... used to indicate where to merge
+// Sebagai permulaan, di bawah ini merupakan sebuah _variadic tuple_ yang
+// akan selalu memberikan prefiks pada _tuple_ lain dengan sebuah bilangan:
 
-// This can then be used for composition:
+type AddMax<T extends unknown[]> = [max: number, ...rest: T];
+//          ^ Tipe generik digunakan untuk
+//            membatasi T
+//                                                ^ Operator `...` digunakan
+//                                                  untuk mengetahui letak
+//                                                  penggabungan
+
+// Tipe di atas dapat digunakan untuk komposisi:
 type MaxMin = AddMax<[min: number]>
 type MaxMinDiameter = AddMax<[min: number, diameter: number]>
 
-// The same can be used after the tuple:
+// Hal yang sama juga digunakan setelah penggunaan _tuple_:
 type SuffixDIContext<T extends unknown[]> = [...first: T, context: any];
 type DIContainer = SuffixDIContext<[param: string]>
 
-// This mechanism can be combined with multiple input params. For example, This
-// function merges two arrays but uses '\0' as a sigil to indicate where the arrays 
-// start and stop.
+// Mekanisme ini dapat digabungkan dengan banyak parameter masukan.
+// Sebagai contoh, fungsi di bawah ini menggabungkan dua buah _array_
+// namun menggunakan '\0' sebagai karakter yang menjadi tanda
+// dimana _array_ dimulai dan berakhir.
 function joinWithNullTerminators<T extends unknown[], U extends unknown[]>(t: [...T], u: [...U]) {
     return ['\0', ...t, '\0', ...u, '\0'] as const;
 }
 
-// TypeScript can infer the return type of a function like this:
+// TypeScript dapat menyimpilkan tipe kembalian dari sebuah fungsi
+// seperti berikut:
 const result = joinWithNullTerminators(['variadic', 'types'], ["terminators", 3]);
 
 //
-// These tools make it possible to correctly type a function like curry which
-// is a well used concept in functional programming:
+// Perkakas ini memungkinkan kita untuk memberi tipe
+// pada sebuah fungsi seperti fungsi untuk _currying_
+// yang merupakan sebuah konsep yang sering digunakan
+// pada pemrograman fungsional:
 
 function curry<T extends unknown[], U extends unknown[], R>(f: (...args: [...T, ...U]) => R, ...a: T) {
     return (...b: U) => f(...a, ...b);
 }
 
-// There are three generic arguments:
-// - T: The params which are array of inputs to the curry function
-// - U: The parameters which _aren't_ passed into to curry function, and need applying to the return func
-// - R: the return type of the passed in function
+// Ada 3 parameter generik:
+// - T: Kumpulan masukan yang digunakan pada fungsi _curry_
+// - U: Kumpulan masukan yang tidak digunakan pada fungsi
+//      _curry_, dan harus diteruskan pada fungsi yang dikembalikan
+// - R: Tipe kembalian dari fungsi yang diberikan
 
 const sum = (left: number, right: number,) => left + right
 
@@ -49,6 +60,7 @@ const a = curry(sum, 1, 2)
 const b = curry(sum, 1)(2)
 const c = curry(sum)(1, 2)
 
-// You can find a more indepth explanation, with more code samples in
+// Anda dapat mempelajari lebih lanjut tentang _variadic tuples_,
+// beserta dengan contoh kode programnya pada:
 // https://github.com/microsoft/TypeScript/pull/39094
- 
+
