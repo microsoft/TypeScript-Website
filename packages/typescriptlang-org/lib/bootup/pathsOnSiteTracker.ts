43d7d5887f1ffeea27e7456285772facd077e98e
Orta <git@orta.io>
2020-10-30 11:38:39 -0400

    Make the generated folder if needed

--- packages/typescriptlang-org/lib/bootup/pathsOnSiteTracker.ts
+++ packages/typescriptlang-org/lib/bootup/pathsOnSiteTracker.ts
@@ -1,16 +1,20 @@
-import { writeFileSync } from "fs"
+import { existsSync, mkdirSync, writeFileSync } from "fs"
 import { join } from "path"
 
 const paths = [] as string[]
 export const addPathToSite = (path: string) => paths.push(path)
 
 export const writeAllPathsToFixture = () => {
-  // prettier-ignore
-  const allPagesPath =  join(__dirname, "..", "..", "src", "__generated__", "allPages.ts")
+  const generated = join(__dirname, "..", "..", "src", "__generated__")
+  if (!existsSync(generated)) {
+    mkdirSync(generated)
+  }
+
+  const allPagesPath = join(generated, "allPages.ts")
   writeFileSync(
     allPagesPath,
     `
-// Generated during bootstapping via onPostBuild.ts
+// Generated during bootstapping via pathsOnSiteTracker.ts
   
 export const allFiles = ["${paths.join('", "')}"]`
   )
