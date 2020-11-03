2c8ff4f8627a12ee44e175decd5c31fae0ad56f0
Orta <git@orta.io>
2020-11-03 07:57:27 -0500

    Update shiki

--- packages/shiki-twoslash/src/index.ts
+++ packages/shiki-twoslash/src/index.ts
@@ -1,6 +1,6 @@
 import { loadTheme, getHighlighter, getTheme } from "shiki"
 import { Highlighter } from "shiki/dist/highlighter"
-import { commonLangIds, commonLangAliases, otherLangIds } from "shiki-languages"
+import { BUNDLED_LANGUAGES } from "shiki-languages"
 import { twoslasher, TwoSlashOptions, TwoSlashReturn } from "@typescript/twoslash"
 import { createDefaultMapFromNodeModules, addAllFilesFromFolder } from "@typescript/vfs"
 import { twoslashRenderer } from "./renderers/twoslash"
@@ -13,10 +13,9 @@ export type ShikiTwoslashSettings = {
   nodeModulesTypesPath?: string
 }
 
-const languages = [...commonLangIds, ...commonLangAliases, ...otherLangIds]
-
 /** Checks if a particular lang is available in shiki */
-export const canHighlightLang = (lang: string) => languages.includes(lang as any)
+export const canHighlightLang = (lang: string) =>
+  BUNDLED_LANGUAGES.find(l => l.aliases?.includes(lang) || l.id === lang)
 
 /**
  * This gets filled in by the promise below, then should
@@ -49,7 +48,7 @@ export const createShikiHighlighter = (options: import("shiki/dist/highlighter")
     }
   }
 
-  return getHighlighter({ theme: shikiTheme, langs: languages }).then(newHighlighter => {
+  return getHighlighter({ theme: shikiTheme, langs: BUNDLED_LANGUAGES }).then(newHighlighter => {
     storedHighlighter = newHighlighter
     return storedHighlighter
   })
