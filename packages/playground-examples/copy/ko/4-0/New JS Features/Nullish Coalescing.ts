7cce659bbd9129373c7d3b96482494c80c827a4e
mhkim <yahma25@gmail.com>
2020-12-28 20:47:48 +0900

    Translation 2 file to Ko

---
+++ packages/playground-examples/copy/ko/4-0/New JS Features/Nullish Coalescing.ts
@@ -0,0 +1,53 @@
+// # Nullish Coalescing
+//
+// This is a new operator `??` which is meant to augment
+// the common usage of `||` in the same way `===` augments `==`
+// as a more strict form of equality.
+// `??`는 `||`의 일반적인 사용 방법을 보완하는 새로운 연산자입니다.
+// 같은 방식으로 `===`가 `==`의 사용을 더 엄격한 등호 형태로
+// 보완시킵니다.
+//
+// To understand it, let's look a how || works:
+// 방식을 이해하기 위해 어떻게 ||가 동작하는지 봅시다:
+
+const response = {
+  nullValue: null,
+  headerText: "",
+  animationDuration: 0,
+  height: 400,
+  showSplashScreen: false,
+} as const;
+
+const undefinedValue = response.undefinedValue || "some other default";
+// This would be: 'some other default'
+// 결과는 이렇게 될 겁니다: 'some other default'
+
+const nullValue = response.nullValue || "some other default";
+
+// These two examples work similar in most languages. As a tool || is
+// pretty good at defaulting things, but JavaScript's falsy checks
+// can surprise you for some common values:
+// 2개의 예시는 대부분 언어에서 비슷하게 동작합니다.
+// 하나의 도구로써 || 연산자는 기본값을 설정할 때 매우 적합하지만,
+// JavaScript falsy 체크는 몇 가지 일반적인 값으로 여러분을 놀라게 할 수 있습니다:
+
+// Potentially unintended. '' is falsy, result: 'Hello, world!'
+// 의도된 것이 아닐 수 있습니다. ''은 falsy고, 결과는 다음과 같습니다: 'Hello, world!'
+const headerText = response.headerText || "Hello, world!";
+
+// Potentially unintended. 0 is falsy, result: 300
+// 의도된 것이 아닐 수 있습니다. 0은 falsy고, 결과는 다음과 같습니다: 300
+const animationDuration = response.animationDuration || 300;
+
+// Potentially unintended. false is falsy, result: true
+// 의도된 것이 아닐 수 있습니다. false는 falsy고, 결과는 다음과 같습니다: true
+const showSplashScreen = response.showSplashScreen || true;
+
+// When switching to use ?? instead, then === equality is used
+// to compare the two sides:
+// 대신 ??으로 전환하여 사용한다면,
+// === 등호는 양쪽을 비교하기 위해 사용됩니다:
+
+const emptyHeaderText = response.headerText ?? "Hello, world!";
+const zeroAnimationDuration = response.animationDuration ?? 300;
+const skipSplashScreen = response.showSplashScreen ?? true;
