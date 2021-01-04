7033346fabac70fa02a9fb7e80a2be7b74fd36fa
Orta <git@orta.io>
2021-01-04 14:43:51 +0000

    Remove auto-select from modals in the playground

--- packages/playground/src/createUI.ts
+++ packages/playground/src/createUI.ts
@@ -83,7 +83,11 @@ export const createUI = (): UI => {
     event?: React.MouseEvent
   ) => {
     const modal = createModalOverlay(postFocalElement)
-    const isNotMouse = event && event.screenX === 0 && event.screenY === 0
+    // I've not been able to get this to work in a way which
+    // works with every screenreader and browser combination, so
+    // instead I'm dropping the feature.
+
+    const isNotMouse = false //  event && event.screenX === 0 && event.screenY === 0
 
     if (subtitle) {
       const titleElement = document.createElement("h3")
