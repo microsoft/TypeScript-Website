684d302cc540850375fc25ca1e9f0fdc7e2918cc
Orta <git@orta.io>
2021-01-05 18:26:25 +0000

    Another look at fixing the appinights exception

--- packages/typescriptlang-org/gatsby-browser.js
+++ packages/typescriptlang-org/gatsby-browser.js
@@ -19,29 +19,29 @@ exports.onRouteUpdate = ({ location, prevLocation }) => {
     // loggingLevelConsole: 1
   }
 
-  // prettier-ignore
-  // @ts-ignore
-  var aiName = window[sdkInstance], aisdk = window[aiName] || function (e) { function n(e) { t[e] = function () { var n = arguments; t.queue.push(function () { t[e].apply(t, n) }) } } var t = { config: e }; t.initialize = !0; var i = document, a = window; setTimeout(function () { var n = i.createElement("script"); n.async = true; n.src = e.url || "https://az416426.vo.msecnd.net/scripts/b/ai.2.min.js", i.getElementsByTagName("script")[0].parentNode.appendChild(n) }); try { t.cookie = i.cookie } catch (e) { } t.queue = [], t.version = 2; for (var r = ["Event", "PageView", "Exception", "Trace", "DependencyData", "Metric", "PageViewPerformance"]; r.length;)n("track" + r.pop()); n("startTrackPage"), n("stopTrackPage"); var s = "Track" + r[0]; if (n("start" + s), n("stop" + s), n("setAuthenticatedUserContext"), n("clearAuthenticatedUserContext"), n("flush"), !(!0 === e.disableExceptionTracking || e.extensionConfig && e.extensionConfig.ApplicationInsightsAnalytics && !0 === e.extensionConfig.ApplicationInsightsAnalytics.disableExceptionTracking)) { n("_" + (r = "onerror")); var o = a[r]; a[r] = function (e, n, i, a, s) { var c = o && o(e, n, i, a, s); return !0 !== c && t["_" + r]({ message: e, url: n, lineNumber: i, columnNumber: a, error: s }), c }, e.autoExceptionInstrumented = !0 } return t }(config);
-  window[aiName] = aisdk
-
-  const locationWithoutPlaygroundCode = location.pathname
-    .split("#code")[0]
-    .split("#src")[0]
-
-  const prevHref = (prevLocation && prevLocation.pathname) || ""
-  const previousLocationWithoutPlaygroundCode = prevHref
-    .split("#code")[0]
-    .split("#src")[0]
-
-  const referrerWithoutPlaygroundCode =
-    document.referrer && document.referrer.split("#code")[0].split("#src")[0]
-
   let hasLocalStorage = false
   try {
     hasLocalStorage = typeof localStorage !== `undefined`
   } catch (error) {}
 
   try {
+    // prettier-ignore
+    // @ts-ignore
+    var aiName = window[sdkInstance], aisdk = window[aiName] || function (e) { function n(e) { t[e] = function () { var n = arguments; t.queue.push(function () { t[e].apply(t, n) }) } } var t = { config: e }; t.initialize = !0; var i = document, a = window; setTimeout(function () { var n = i.createElement("script"); n.async = true; n.src = e.url || "https://az416426.vo.msecnd.net/scripts/b/ai.2.min.js", i.getElementsByTagName("script")[0].parentNode.appendChild(n) }); try { t.cookie = i.cookie } catch (e) { } t.queue = [], t.version = 2; for (var r = ["Event", "PageView", "Exception", "Trace", "DependencyData", "Metric", "PageViewPerformance"]; r.length;)n("track" + r.pop()); n("startTrackPage"), n("stopTrackPage"); var s = "Track" + r[0]; if (n("start" + s), n("stop" + s), n("setAuthenticatedUserContext"), n("clearAuthenticatedUserContext"), n("flush"), !(!0 === e.disableExceptionTracking || e.extensionConfig && e.extensionConfig.ApplicationInsightsAnalytics && !0 === e.extensionConfig.ApplicationInsightsAnalytics.disableExceptionTracking)) { n("_" + (r = "onerror")); var o = a[r]; a[r] = function (e, n, i, a, s) { var c = o && o(e, n, i, a, s); return !0 !== c && t["_" + r]({ message: e, url: n, lineNumber: i, columnNumber: a, error: s }), c }, e.autoExceptionInstrumented = !0 } return t }(config);
+    window[aiName] = aisdk
+
+    const locationWithoutPlaygroundCode = location.pathname
+      .split("#code")[0]
+      .split("#src")[0]
+
+    const prevHref = (prevLocation && prevLocation.pathname) || ""
+    const previousLocationWithoutPlaygroundCode = prevHref
+      .split("#code")[0]
+      .split("#src")[0]
+
+    const referrerWithoutPlaygroundCode =
+      document.referrer && document.referrer.split("#code")[0].split("#src")[0]
+
     // @ts-ignore
     aisdk.trackPageView({
       uri: locationWithoutPlaygroundCode,
