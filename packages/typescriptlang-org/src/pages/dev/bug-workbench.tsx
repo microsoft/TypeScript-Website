0cf904bf25d7cf6408d67d8a8ab9f7477a1c7942
Orta <git@orta.io>
2021-01-21 09:56:08 +0000

    Save the JSX params before a reload

--- packages/typescriptlang-org/src/pages/dev/bug-workbench.tsx
+++ packages/typescriptlang-org/src/pages/dev/bug-workbench.tsx
@@ -111,6 +111,7 @@ const Play: React.FC<Props> = (props) => {
             dtsMap = defaultMap
             runTwoslash()
           })
+
         }
 
         // When the compiler notices a twoslash compiler flag change, this will get triggered and reset the DTS map
