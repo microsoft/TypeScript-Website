822ab57ab3ab59297682850d09f1a624cbadb4a5
Orta Therox <git@orta.io>
2021-02-16 07:29:15 +0000

    Get green

--- packages/shiki-twoslash/test/tsconfig-renderer.test.ts
+++ packages/shiki-twoslash/test/tsconfig-renderer.test.ts
@@ -16,18 +16,9 @@ describe("with a simple example", () => {
     const tokens = highlighter.codeToThemedTokens(code, "json")
     const html = renderers.tsconfigJSONRenderer(tokens, {})
 
-    expect(html).toMatchInlineSnapshot(`
-      "<pre class=\\"shiki tsconfig lsp\\"><div class='code-container'><code>
-      <span style=\\"color: #D4D4D4\\">{</span>
-      <span style=\\"color: #D4D4D4\\">  </span><span style=\\"color: #9CDCFE\\">\\"<a aria-hidden=true href='https://www.typescriptlang.org/tsconfig#compilerOptions'><data-lsp lsp=\\"The set of compiler options for your project\\">compilerOptions</data-lsp></a>\\"</span><span style=\\"color: #D4D4D4\\">: {</span>
-      <span style=\\"color: #D4D4D4\\">    </span><span style=\\"color: #9CDCFE\\">\\"<a aria-hidden=true href='https://www.typescriptlang.org/tsconfig#module'><data-lsp lsp=\\"Specify what module code is generated.\\">module</data-lsp></a>\\"</span><span style=\\"color: #D4D4D4\\">: </span><span style=\\"color: #CE9178\\">\\"commonjs\\"</span>
-      <span style=\\"color: #D4D4D4\\">  },</span>
-      <span style=\\"color: #D4D4D4\\">  </span><span style=\\"color: #9CDCFE\\">\\"<a aria-hidden=true href='https://www.typescriptlang.org/tsconfig#files'><data-lsp lsp=\\"Include a list of files. This does not support glob patterns, as opposed to \`include\`.\\">files</data-lsp></a>\\"</span><span style=\\"color: #D4D4D4\\">: [</span>
-      <span style=\\"color: #D4D4D4\\">    </span><span style=\\"color: #CE9178\\">\\"core.ts\\"</span>
-      <span style=\\"color: #D4D4D4\\">  ]</span>
-      <span style=\\"color: #D4D4D4\\">}</span>
-      <span style=\\"color: #D4D4D4\\">    </span></code></div></pre>"
-    `)
+    expect(html).toMatchInlineSnapshot(
+      `"<pre class=\\"shiki tsconfig lsp\\"><div class='code-container'><code><div class='line'></div><div class='line'><span style=\\"color: #D4D4D4\\">{</span></div><div class='line'><span style=\\"color: #D4D4D4\\">  </span><span style=\\"color: #9CDCFE\\">\\"<a aria-hidden=true href='https://www.typescriptlang.org/tsconfig#compilerOptions'><data-lsp lsp=\\"The set of compiler options for your project\\">compilerOptions</data-lsp></a>\\"</span><span style=\\"color: #D4D4D4\\">: {</span></div><div class='line'><span style=\\"color: #D4D4D4\\">    </span><span style=\\"color: #9CDCFE\\">\\"<a aria-hidden=true href='https://www.typescriptlang.org/tsconfig#module'><data-lsp lsp=\\"Specify what module code is generated.\\">module</data-lsp></a>\\"</span><span style=\\"color: #D4D4D4\\">: </span><span style=\\"color: #CE9178\\">\\"commonjs\\"</span></div><div class='line'><span style=\\"color: #D4D4D4\\">  },</span></div><div class='line'><span style=\\"color: #D4D4D4\\">  </span><span style=\\"color: #9CDCFE\\">\\"<a aria-hidden=true href='https://www.typescriptlang.org/tsconfig#files'><data-lsp lsp=\\"Include a list of files. This does not support glob patterns, as opposed to \`include\`.\\">files</data-lsp></a>\\"</span><span style=\\"color: #D4D4D4\\">: [</span></div><div class='line'><span style=\\"color: #D4D4D4\\">    </span><span style=\\"color: #CE9178\\">\\"core.ts\\"</span></div><div class='line'><span style=\\"color: #D4D4D4\\">  ]</span></div><div class='line'><span style=\\"color: #D4D4D4\\">}</span></div><div class='line'><span style=\\"color: #D4D4D4\\">    </span></div></code></div></pre>"`
+    )
   })
 })
 
