8fc7a9aa6c6d4eeb6f07a9ef54e4eb19de29ab75
Orta <git@orta.io>
2021-03-05 12:35:47 +0000

    Drastic improvements in pdf rendering

--- packages/handbook-epub/script/createEpub.ts
+++ packages/handbook-epub/script/createEpub.ts
@@ -6,61 +6,29 @@
 
 const jetpack = require("fs-jetpack");
 const { createReadStream } = jetpack;
-
 const Streampub = require("streampub");
-const toHAST = require(`mdast-util-to-hast`);
-const hastToHTML = require(`hast-util-to-html`);
-const {
-  recursiveReadDirSync,
-} = require("../../typescriptlang-org/lib/utils/recursiveReadDirSync");
 
-import {
-  readdirSync,
-  readFileSync,
-  lstatSync,
-  copyFileSync,
-  mkdirSync,
-} from "fs";
-import runTwoSlashAcrossDocument from "gatsby-remark-shiki-twoslash";
-const remark = require("remark");
+import { copyFileSync, mkdirSync } from "fs";
 import { join } from "path";
-import { read as readMarkdownFile } from "gray-matter";
-
-import { getDocumentationNavForLanguage } from "../../typescriptlang-org/src/lib/documentationNavigation";
 import { exists } from "fs-jetpack";
+import {
+  generateV2Markdowns,
+  getGitSHA,
+  getHTML,
+  getReleaseInfo,
+  replaceAllInString,
+} from "./setupPages";
+import { getDocumentationNavForLanguage } from "../../typescriptlang-org/src/lib/documentationNavigation";
 
 // Reference: https://github.com/AABoyles/LessWrong-Portable/blob/master/build.js
 
-const markdowns = new Map<string, ReturnType<typeof readMarkdownFile>>();
-const handbookNavigation = getDocumentationNavForLanguage("en");
-
-// Grab all the md + yml info from the handbook files on disk
-// and add them to ^
-// prettier-ignore
-const handbookPath = join( __dirname, "..", "..", "documentation", "copy", "en", "handbook-v2");
-
-recursiveReadDirSync(handbookPath).forEach((path) => {
-  const filePath = join(__dirname, "..", "..", path);
-  if (lstatSync(filePath).isDirectory() || !filePath.endsWith("md")) {
-    return;
-  }
-
-  const md = readMarkdownFile(filePath);
-  // prettier-ignore
-  if (!md.data.permalink) {
-    throw new Error(
-      `${filePath} in the handbook did not have a permalink in the yml header`,
-    );
-  }
-  const id = md.data.permalink;
-  markdowns.set(id, md);
-});
+const markdowns = generateV2Markdowns();
 
 // Grab the handbook nav, and use that to pull out the order
 
 const bookMetadata = {
   title: "TypeScript Handbook",
-  author: "TypeScript Contributors",
+  author: "TypeScript Team and Open Source Contributors",
   authorUrl: "https://www.typescriptlang.org/",
   modified: new Date(),
   source: "https://www.typescriptlang.org",
@@ -77,6 +45,8 @@ if (!exists(dist)) mkdirSync(dist);
 const epubPath = join(dist, "handbook.epub");
 
 const startEpub = async () => {
+  const handbookNavigation = getDocumentationNavForLanguage("en");
+
   const handbook = handbookNavigation.find((i) => i.title === "Handbook");
   const epub = new Streampub(bookMetadata);
 
@@ -124,26 +94,12 @@ const startEpub = async () => {
   epub.end();
 };
 
-// The epub generation is async-y, so just wait till everything is
-// done to move over the file into the website's static assets.
-process.once("exit", () => {
-  copyFileSync(
-    epubPath,
-    join(
-      __dirname,
-      "../../typescriptlang-org/static/assets/typescript-handbook.epub"
-    )
-  );
-});
-
 const addHandbookPage = async (epub: any, id: string, index: number) => {
   const md = markdowns.get(id);
   if (!md)
-    throw new Error(
-      "Could not get markdown for " +
-        id +
-        `\n\nAll MDs: ${Array.from(markdowns.keys())}`
-    );
+    // prettier-ignore
+    throw new Error("Could not get markdown for " + id + `\n\nAll MDs: ${Array.from(markdowns.keys())}`);
+
   const title = md.data.title;
   const prefix = `<link href="style.css" type="text/css" rel="stylesheet" /><h1>${title}</h1><div class='section'>`;
   const suffix = "</div>";
@@ -155,57 +111,14 @@ const addHandbookPage = async (epub: any, id: string, index: number) => {
   epub.write(Streampub.newChapter(title, prefix + edited + suffix, index));
 };
 
-const getHTML = async (code: string, settings?: any) => {
-  const markdownAST: Node = remark().parse(code);
-
-  // Basically, only run with twoslash during prod builds
-  if (process.env.CI) {
-    await runTwoSlashAcrossDocument(
-      { markdownAST },
-      {
-        theme: require("../../typescriptlang-org/lib/themes/typescript-beta-light.json"),
-      },
-      // @ts-ignore
-      {}
-    );
-  }
-
-  const hAST = toHAST(markdownAST, { allowDangerousHTML: true });
-  return hastToHTML(hAST, { allowDangerousHTML: true });
-};
-
-function replaceAllInString(_str: string, obj: any) {
-  let str = _str;
-
-  Object.keys(obj).forEach((before) => {
-    str = str.replace(new RegExp(before, "g"), obj[before]);
-  });
-  return str;
-}
-
-const getGitSHA = () => {
-  const gitRoot = join(__dirname, "..", "..", "..", ".git");
-  const rev = readFileSync(join(gitRoot, "HEAD"), "utf8").trim();
-  if (rev.indexOf(":") === -1) {
-    return rev;
-  } else {
-    return readFileSync(join(gitRoot, rev.substring(5)), "utf8");
-  }
-};
-
-const getReleaseInfo = () => {
-  // prettier-ignore
-  const releaseInfo = join(
-    __dirname,
-    "..",
-    "..",
-    "typescriptlang-org",
-    "src",
-    "lib",
-    "release-info.json",
-  );
-  const info = JSON.parse(readFileSync(releaseInfo, "utf8"));
-  return info;
-};
-
 startEpub();
+
+// The epub generation is async-y, so just wait till everything is
+// done to move over the file into the website's static assets.
+process.once("exit", () => {
+  copyFileSync(
+    epubPath,
+    // prettier-ignore
+    join(__dirname, "../../typescriptlang-org/static/assets/typescript-handbook.epub")
+  );
+});
---
+++ packages/handbook-epub/script/setupPages.ts
@@ -0,0 +1,104 @@
+#!/usr/bin/env ts-node
+
+/* With twoslash
+   env CI=213 yarn workspace handbook-epub build
+*/
+
+const jetpack = require("fs-jetpack");
+const { createReadStream } = jetpack;
+
+const toHAST = require(`mdast-util-to-hast`);
+const hastToHTML = require(`hast-util-to-html`);
+const {
+  recursiveReadDirSync,
+} = require("../../typescriptlang-org/lib/utils/recursiveReadDirSync");
+
+import {
+  readdirSync,
+  readFileSync,
+  lstatSync,
+  copyFileSync,
+  mkdirSync,
+} from "fs";
+import runTwoSlashAcrossDocument from "gatsby-remark-shiki-twoslash";
+const remark = require("remark");
+import { join } from "path";
+import { read as readMarkdownFile } from "gray-matter";
+
+import { getDocumentationNavForLanguage } from "../../typescriptlang-org/src/lib/documentationNavigation";
+import { exists } from "fs-jetpack";
+
+// Reference: https://github.com/AABoyles/LessWrong-Portable/blob/master/build.js
+
+export const generateV2Markdowns = () => {
+  const markdowns = new Map<string, ReturnType<typeof readMarkdownFile>>();
+
+  // Grab all the md + yml info from the handbook files on disk
+  // and add them to ^
+  // prettier-ignore
+  const handbookPath = join( __dirname, "..", "..", "documentation", "copy", "en", "handbook-v2");
+
+  recursiveReadDirSync(handbookPath).forEach((path) => {
+    const filePath = join(__dirname, "..", "..", path);
+    if (lstatSync(filePath).isDirectory() || !filePath.endsWith("md")) {
+      return;
+    }
+
+    const md = readMarkdownFile(filePath);
+    // prettier-ignore
+    if (!md.data.permalink) {
+      throw new Error(
+        `${filePath} in the handbook did not have a permalink in the yml header`,
+      );
+    }
+    const id = md.data.permalink;
+    markdowns.set(id, md);
+  });
+
+  return markdowns;
+};
+
+export const getHTML = async (code: string, settings?: any) => {
+  const markdownAST: Node = remark().parse(code);
+
+  // Basically, only run with twoslash during prod builds
+  if (process.env.CI) {
+    await runTwoSlashAcrossDocument(
+      { markdownAST },
+      {
+        theme: require("../../typescriptlang-org/lib/themes/typescript-beta-light.json"),
+      },
+      // @ts-ignore
+      {}
+    );
+  }
+
+  const hAST = toHAST(markdownAST, { allowDangerousHTML: true });
+  return hastToHTML(hAST, { allowDangerousHTML: true });
+};
+
+export function replaceAllInString(_str: string, obj: any) {
+  let str = _str;
+
+  Object.keys(obj).forEach((before) => {
+    str = str.replace(new RegExp(before, "g"), obj[before]);
+  });
+  return str;
+}
+
+export const getGitSHA = () => {
+  const gitRoot = join(__dirname, "..", "..", "..", ".git");
+  const rev = readFileSync(join(gitRoot, "HEAD"), "utf8").trim();
+  if (rev.indexOf(":") === -1) {
+    return rev;
+  } else {
+    return readFileSync(join(gitRoot, rev.substring(5)), "utf8");
+  }
+};
+
+export const getReleaseInfo = () => {
+  // prettier-ignore
+  const releaseInfo = join(__dirname, "..", "..", "typescriptlang-org", "src", "lib", "release-info.json");
+  const info = JSON.parse(readFileSync(releaseInfo, "utf8"));
+  return info;
+};
