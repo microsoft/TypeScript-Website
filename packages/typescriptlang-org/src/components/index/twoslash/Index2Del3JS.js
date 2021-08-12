5d131f6f957cb1d5969d8337099102a3a66003a0
Orta <git@orta.io>
2021-08-12 19:23:48 +0100

    More work

--- packages/typescriptlang-org/src/components/index/twoslash/Index2Del3JS.js
+++ packages/typescriptlang-org/src/components/index/twoslash/Index2Del3JS.js
@@ -1,6 +1,7 @@
 // twoslash: { themes: ["../../../packages/typescriptlang-org/lib/themes/typescript-beta-light"] }
 
 
+
 function verify(result) {
   if (result === "pass") {
     console.log("Passed")
