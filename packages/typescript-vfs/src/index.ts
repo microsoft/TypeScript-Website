920a54021725d2412f7963f93ec39fe4daeb37f1
Rongjian Zhang <pd4d10@gmail.com>
2021-09-19 23:29:34 +0800

    Make it compatible with environments without localStorage

--- packages/typescript-vfs/src/index.ts
+++ packages/typescript-vfs/src/index.ts
@@ -268,7 +268,6 @@ export const createDefaultMapFromCDN = (
   storer?: typeof localStorage
 ) => {
   const fetchlike = fetcher || fetch
-  const storelike = storer || localStorage
   const fsMap = new Map<string, string>()
   const files = knownLibFilesForCompilerOptions(options, ts)
   const prefix = `https://typescript.azureedge.net/cdn/${version}/typescript/lib/`
@@ -290,6 +289,8 @@ export const createDefaultMapFromCDN = (
 
   // A localstorage and lzzip aware version of the lib files
   function cached() {
+    const storelike = storer || localStorage
+
     const keys = Object.keys(localStorage)
     keys.forEach(key => {
       // Remove anything which isn't from this version
