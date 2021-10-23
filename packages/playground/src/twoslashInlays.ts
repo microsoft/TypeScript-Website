ba72e492dec8e851b236b8c5e4f804c86e806acf
Orta Therox <git@orta.io>
2021-10-23 21:32:34 +0100

    Update twoslashInlays.ts

--- packages/playground/src/twoslashInlays.ts
+++ packages/playground/src/twoslashInlays.ts
@@ -22,7 +22,6 @@ export const createTwoslashInlayProvider = (sandbox: Sandbox) => {
 
         const hint = await worker.getQuickInfoAtPosition("file://" + model.uri.path, inspectionOff)
         if (!hint || !hint.displayParts) continue
-        console.log({ hint })
 
         const inlay: import("monaco-editor").languages.InlayHint = {
           // @ts-ignore
@@ -38,48 +37,3 @@ export const createTwoslashInlayProvider = (sandbox: Sandbox) => {
   }
   return provider
 }
-
-// class InlayHintsAdapter extends Adapter implements import("monaco-editor").editor.languages.InlayHintsProvider {
-//   public async provideInlayHints(
-//     model: import("monaco-editor").ITextModel,
-//     range: Range,
-//     token: CancellationToken
-//   ): Promise<languages.InlayHint[]> {
-//     const resource = model.uri
-//     const fileName = resource.toString()
-//     const start = model.getOffsetAt({
-//       lineNumber: range.startLineNumber,
-//       column: range.startColumn,
-//     })
-
-//     const end = model.getOffsetAt({
-//       lineNumber: range.endLineNumber,
-//       column: range.endColumn,
-//     })
-//     const worker = await this._worker(resource)
-//     if (model.isDisposed()) {
-//       return []
-//     }
-
-//     const hints = await worker.provideInlayHints(fileName, start, end)
-
-//     return hints.map(hint => {
-//       return {
-//         ...hint,
-//         position: model.getPositionAt(hint.position),
-//         kind: this._convertHintKind(hint.kind),
-//       }
-//     })
-//   }
-
-//   private _convertHintKind(kind?: ts.InlayHintKind) {
-//     switch (kind) {
-//       case "Parameter":
-//         return languages.InlayHintKind.Parameter
-//       case "Type":
-//         return languages.InlayHintKind.Type
-//       default:
-//         return languages.InlayHintKind.Other
-//     }
-//   }
-// }
