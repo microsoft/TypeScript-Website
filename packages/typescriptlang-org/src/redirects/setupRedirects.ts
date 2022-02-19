b4121598e0e9b319b9a2c8f447263ce29c0184c9
soartec-lab <info@soartec-lab.work>
2022-02-19 19:19:44 +0900

    Add `typings-for-npm-packages.html` into redirect paths

--- packages/typescriptlang-org/src/redirects/setupRedirects.ts
+++ packages/typescriptlang-org/src/redirects/setupRedirects.ts
@@ -16,6 +16,7 @@ export const handbookRedirects = {
   "/docs/handbook/writing-declaration-files.html": "/docs/handbook/declaration-files/introduction.html",
   "/docs/handbook/writing-definition-files": "/docs/handbook/declaration-files/introduction.html",
   "/docs/handbook/typings-for-npm-packages": "/docs/handbook/declaration-files/publishing.html",
+  "/docs/handbook/typings-for-npm-packages.html": "/docs/handbook/declaration-files/publishing.html",
   "/docs/handbook/release-notes": "/docs/handbook/release-notes/overview.html",
   "/docs/tutorial.html": "/docs/handbook/release-notes/overview.html",
   "/docs/handbook/release-notes/overview": "/docs/handbook/release-notes/overview.html",
