ec93a379d25d54212d751d2131d7634ca1ecc919
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-07-22 12:11:37 +0100

    resolve imports with filepath in module specifier

--- packages/ata/tests/edgeCases.spec.ts
+++ packages/ata/tests/edgeCases.spec.ts
@@ -1,11 +1,16 @@
 import { mapModuleNameToModule } from "../src/edgeCases"
 
 describe(mapModuleNameToModule, () => {
-    it("gives node for known identifiers", () => {
-        expect(mapModuleNameToModule("fs")).toEqual("node")
-    })
+  it("gives node for known identifiers", () => {
+    expect(mapModuleNameToModule("fs")).toEqual("node")
+  })
 
-    it("handles the weird node: prefix", () => {
-        expect(mapModuleNameToModule("node:fs")).toEqual("node")
-    })
+  it("handles the weird node: prefix", () => {
+    expect(mapModuleNameToModule("node:fs")).toEqual("node")
+  })
+
+  it("strips module filepaths", () => {
+    expect(mapModuleNameToModule("lodash/identity")).toEqual("lodash")
+    expect(mapModuleNameToModule("@org/lodash/identity")).toEqual("@org/lodash")
+  })
 })
