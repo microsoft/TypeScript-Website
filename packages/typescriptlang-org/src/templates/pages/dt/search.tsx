e0ebae2058af92a039f91052a7a00c7c294a527f
Josh Goldberg <git@joshuakgoldberg.com>
2023-01-18 12:42:20 -0600

    Un-localized the page

--- packages/typescriptlang-org/src/templates/pages/dt/search.tsx
+++ packages/typescriptlang-org/src/templates/pages/dt/search.tsx
@@ -1,29 +1,29 @@
 import React from "react"
-import { useIntl } from "react-intl";
 
 import { Layout } from "../../../components/layout"
 import { Intl } from "../../../components/Intl"
-import { createInternational } from "../../../lib/createInternational"
-import { dtCopy } from "../../../copy/en/dt"
 
 type SearchProps = {
   pageContext: { lang: string }
 }
 
+const title = "Search for typed packages";
+const description = "This page is no longer necessary.";
+
 const Search: React.FC<SearchProps> = ({ pageContext }) => {
-  const intl = useIntl()
-  const i = createInternational<typeof dtCopy>(intl)
-
-  const title = i("dt_s_page_title");
-
   return (
-    <Layout title={title} description={i("dt_s_subtitle")} lang={pageContext.lang}>
+    <Layout title={title} description={description} lang={pageContext.lang}>
       <div className="raised main-content-block">
         <h1>{title}</h1>
         <p>
-          {i("dt_s_read_more")}{' '}
+          <strong>{description}</strong>
+        </p>
+        <p>
+          The npm and Yarn package registries now include type information for packages.
+          <br />
+          You can read more on:{' '}
           <a href="https://github.blog/changelog/2020-12-16-npm-displays-packages-with-bundled-typescript-declarations">
-            {i("dt_s_read_more_link")}
+            npm displays packages with bundled TypeScript declarations
           </a>.
         </p>
       </div>
