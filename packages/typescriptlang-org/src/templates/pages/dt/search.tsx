e0c4a713a5f14cf211d877b92d1bbb59893f656e
Josh Goldberg <git@joshuakgoldberg.com>
2023-01-18 11:08:41 -0600

    Good looking layout

--- packages/typescriptlang-org/src/templates/pages/dt/search.tsx
+++ packages/typescriptlang-org/src/templates/pages/dt/search.tsx
@@ -1,26 +1,32 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
+import { useIntl } from "react-intl";
+
 import { Layout } from "../../../components/layout"
 import { Intl } from "../../../components/Intl"
-
 import { createInternational } from "../../../lib/createInternational"
-import { useIntl } from "react-intl"
 import { dtCopy } from "../../../copy/en/dt"
 
 type SearchProps = {
-  pageContext: any
+  pageContext: { lang: string }
 }
 
 const Search: React.FC<SearchProps> = ({ pageContext }) => {
-  const i = createInternational<typeof dtCopy>(useIntl())
+  const intl = useIntl()
+  const i = createInternational<typeof dtCopy>(intl)
+
+  const title = i("dt_s_page_title");
 
   return (
-    <Layout title={i("dt_s_page_title")} description={i("dt_s_subtitle")} lang={pageContext.lang}>
-      <p>
-        {i("dt_s_read_more")}
-        <a href="https://github.blog/changelog/2020-12-16-npm-displays-packages-with-bundled-typescript-declarations">
-          {i("dt_s_read_more_link")}
-        </a>.
-      </p>
+    <Layout title={title} description={i("dt_s_subtitle")} lang={pageContext.lang}>
+      <div className="raised main-content-block">
+        <h1>{title}</h1>
+        <p>
+          {i("dt_s_read_more")}{' '}
+          <a href="https://github.blog/changelog/2020-12-16-npm-displays-packages-with-bundled-typescript-declarations">
+            {i("dt_s_read_more_link")}
+          </a>.
+        </p>
+      </div>
     </Layout>
   )
 }
