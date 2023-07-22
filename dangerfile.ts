2677049bf717c676b6dbb79a8e9aa77d88084207
orta <git@orta.io>
2023-07-22 05:43:12 +0100

    Adds some infra to warn on files which changed in the PR but aren't accounted for

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,9 +2,16 @@
 // yarn danger pr https://github.com/microsoft/TypeScript-Website/pull/115
 
 import spellcheck from "danger-plugin-spellcheck"
-
-// Blocked on PR deploys, see CI.yml
-// import lighthouse from "danger-plugin-lighthouse"
+import { warn, danger }from "danger"
+import { execSync } from "child_process"
 
 // Spell check all the things
 spellcheck({ settings: "microsoft/TypeScript-Website@spellcheck.json" })
+
+const gitStatus = execSync("git status --porcelain").toString()
+if (gitStatus.includes("M")) {
+    const files = gitStatus.split("\n").filter(f => f.startsWith(" M ")).map(f => f.substr(3))
+    const linksToChangedFiles = danger.github.utils.fileLinks(files)
+    
+    warn(`There are un-staged changes to generated files: \n ${linksToChangedFiles}`)
+}
