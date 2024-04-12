855a105e990e7b70cf0a7e00085f17ca1231fe11
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-04-12 10:33:58 -0700

    bring back typegen plugin

--- packages/typescriptlang-org/gatsby-config.js
+++ packages/typescriptlang-org/gatsby-config.js
@@ -50,6 +50,15 @@ module.exports = {
     // Support for downloading or pre-caching pages, needed for PWAs
     // "gatsby-plugin-offline",
 
+    // Creates TS types for queries during `gatsby dev`
+    {
+      resolve: "gatsby-plugin-typegen",
+      options: {
+        // Ensure it works in a monorepo
+        outputPath: __dirname + "/src/__generated__/gatsby-types.ts",
+      },
+    },
+
     // Support ts/tsx files in src
     "gatsby-plugin-typescript",
     // SEO
