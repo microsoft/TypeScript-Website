b3d29fc33137ae6111606a1b3146789734cc28ea
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-05-30 19:56:22 -0700

    Drop direct use of blob storage account

--- packages/typescriptlang-org/src/templates/play.tsx
+++ packages/typescriptlang-org/src/templates/play.tsx
@@ -80,8 +80,7 @@ const Play: React.FC<Props> = (props) => {
       let tsVersionParam = params.get("ts")
       // handle the nightly lookup 
       if (tsVersionParam && tsVersionParam === "Nightly" || tsVersionParam === "next") {
-        // Avoids the CDN to doubly skip caching
-        const nightlyLookup = await fetch("https://tswebinfra.blob.core.windows.net/indexes/next.json", { cache: "no-cache" })
+        const nightlyLookup = await fetch("https://playgroundcdn.typescriptlang.org/indexes/next.json", { cache: "no-cache" })
         const nightlyJSON = await nightlyLookup.json()
         tsVersionParam = nightlyJSON.version
       }
