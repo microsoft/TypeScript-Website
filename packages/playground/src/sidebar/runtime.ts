7edb74eb255d5e46c7f28cf5ca691440400e3b82
Luka Jeran <lukaw3d@gmail.com>
2024-06-28 04:13:26 +0000

    chore: refactor Playground handling undefined in logs

--- packages/playground/src/sidebar/runtime.ts
+++ packages/playground/src/sidebar/runtime.ts
@@ -218,13 +218,17 @@ function rewireLoggingToElement(
       const nameWithoutObject = name && name === "Object" ? "" : htmlEscape(name)
       const prefix = nameWithoutObject ? `${nameWithoutObject}: ` : ""
 
-      // JSON.stringify omits any keys with a value of undefined. To get around this, we replace undefined with the text __undefined__ and then do a global replace using regex back to keyword undefined
       textRep =
         prefix +
-        JSON.stringify(arg, (_, value) => (value === undefined ? "__undefined__" : value), 2).replace(
-          /"__undefined__"/g,
-          "undefined"
-        )
+        JSON.stringify(
+          arg,
+          (_, value) => {
+            // JSON.stringify omits any keys with a value of undefined. To get around this, we replace undefined with the text __undefined__ and then do a global replace using regex back to keyword undefined
+            if (value === undefined) return "__undefined__"
+            return value
+          },
+          2
+        ).replace(/"__undefined__"/g, "undefined")
 
       textRep = htmlEscape(textRep)
     } else {
