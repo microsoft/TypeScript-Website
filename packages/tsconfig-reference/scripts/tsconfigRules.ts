ad9df41f144a431a84678775609b56028d95703a
Max schwenk <maschwenk@gmail.com>
2025-08-08 15:36:44 -0400

    Adjust to feedback and commit build artifacts

--- packages/tsconfig-reference/scripts/tsconfigRules.ts
+++ packages/tsconfig-reference/scripts/tsconfigRules.ts
@@ -239,7 +239,7 @@ export const defaultsForOptions = {
   ],
   moduleResolution: [
     "`Node10` if [`module`](#module) is `CommonJS`;",
-    "`Node16` if [`module`](#module) is `Node16` or `Node18` or `Node20`;",
+    "`Node16` if [`module`](#module) is `Node16`, `Node18`, or `Node20`;",
     "`NodeNext` if [`module`](#module) is `NodeNext`;",
     "`Bundler` if [`module`](#module) is `Preserve`;",
     "`Classic` otherwise.",
