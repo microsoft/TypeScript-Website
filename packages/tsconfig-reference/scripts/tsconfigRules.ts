35fb7fa35a3d3c91211b556dbd33800704bcca91
dynst <148708712+dynst@users.noreply.github.com>
2025-08-31 00:00:00 +0000

    add missing data for erasableSyntaxOnly (from TS 5.8)

--- packages/tsconfig-reference/scripts/tsconfigRules.ts
+++ packages/tsconfig-reference/scripts/tsconfigRules.ts
@@ -186,6 +186,10 @@ export const relatedTo: [AnOption, AnOption[]][] = [
 
   ["preserveValueImports", ["isolatedModules", "importsNotUsedAsValues", "verbatimModuleSyntax"]],
   ["importsNotUsedAsValues", ["preserveValueImports", "verbatimModuleSyntax"]],
+
+  ["erasableSyntaxOnly", ["isolatedModules", "verbatimModuleSyntax"]],
+  ["isolatedModules", ["erasableSyntaxOnly", "verbatimModuleSyntax"]],
+  ["verbatimModuleSyntax", ["erasableSyntaxOnly", "isolatedModules"]],
 ];
 
 /**
@@ -320,6 +324,7 @@ function formatAllowedValues(type: CommandLineOption["type"]) {
 }
 
 export const releaseToConfigsMap: { [key: string]: AnOption[] } = {
+  "5.8": ["erasableSyntaxOnly"],
   "5.7": ["rewriteRelativeImportExtensions"],
   "5.6": ["strictBuiltinIteratorReturn", "noUncheckedSideEffectImports", "noCheck"],
   "5.5": ["isolatedDeclarations"],
