04082c6aec83bc54f12cb1bd9b72d46e6d8e5a01
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-03-02 10:41:28 -0800

    remove danger, significant audit problems

--- dangerfile.ts
+++
@@ -1,9 +0,0 @@
-// You can test it by running
-// pnpm danger pr https://github.com/microsoft/TypeScript-Website/pull/115
-
-// Blocked on PR deploys, see CI.yml
-// import lighthouse from "danger-plugin-lighthouse"
-
-// Spell check all the things
-const { default: spellcheck } = require("danger-plugin-spellcheck")
-spellcheck({ settings: "microsoft/TypeScript-Website@spellcheck.json" })
